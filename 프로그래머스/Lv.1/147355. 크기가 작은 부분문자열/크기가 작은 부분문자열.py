ff740331044eb2dd4c57a7289fd40bc77b544e1d
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2024-01-12 19:08:31 +0900

    [level Lv.1] Title: 크기가 작은 부분문자열, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -0,0 +1,14 @@
+# 프로그래머스 147355 크기가 작은 부분문자열 LV.1
+def solution(t, p):
+    answer = 0
+    if len(p) == 1:
+        idx = len(t)
+    else:
+        idx = len(t) - len(p) + 1
+
+    for i in range(idx):
+        target = t[i:len(p) + i]
+        if target <= p:
+            answer += 1
+
+    return answer
