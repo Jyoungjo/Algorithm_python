c271ffb081b144bb5ecf323d5aaf3ac3a7502a81
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2024-01-22 18:45:31 +0900

    [level 2] Title: 전화번호 목록, Time: 84.49 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,7 +1,7 @@
+# 프로그래머스 42577 전화번호 목록 LV.2
 def solution(phone_book):
-    answer = True
     phone_book.sort()
     for i in range(len(phone_book)-1):
         if phone_book[i+1].startswith(phone_book[i]):
             return False
-    return answer
+    return True
