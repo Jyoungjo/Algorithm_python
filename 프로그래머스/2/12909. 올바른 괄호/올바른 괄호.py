7d1207d2abb0d9df4448253a0c7edb3e06745cff
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2024-01-26 18:13:53 +0900

    [level 2] Title: 올바른 괄호, Time: 9.23 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,16 @@
+# 프로그래머스 12909 올바른 괄호 LV.2
+def solution(s):
+    answer = True
+    stack = []
+    for c in s:
+        if c == '(':
+            stack.append(c)
+        else:
+            if len(stack) == 0:
+                return False
+            else:
+                stack.pop()
+
+    if len(stack) != 0:
+        return False
+    return True
