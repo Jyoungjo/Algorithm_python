5b2d0aa5db2c5c16b896ded886dc8d36d8cb9efb
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2024-02-02 17:59:15 +0900

    [level 1] Title: 최소직사각형, Time: 7.03 ms, Memory: 11.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,10 +1,10 @@
 # 프로그래머스 86491 최소직사각형 LV.1
 def solution(sizes):
-    rows = []
-    cols = []
-    for size in sizes:
-        if size[0] < size[1]:
-            size[0], size[1] = size[1], size[0]
-        rows.append(size[0])
-        cols.append(size[1])
-    return max(rows) * max(cols)
+    row = 0
+    col = 0
+    for a, b in sizes:
+        if a < b:
+            a, b = b, a
+        row = max(row, a)
+        col = max(col, b)
+    return row * col
