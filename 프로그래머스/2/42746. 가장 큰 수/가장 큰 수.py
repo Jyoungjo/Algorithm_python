7ce0763562895f8f3ccbb031cec2111f1923afe6
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2024-02-02 13:26:47 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,9 +1,4 @@
 # 프로그래머스 42746 가장 큰 수 LV.2
 def solution(numbers):
-    answer = ''
     numbers = sorted(map(str, numbers), key=lambda x: x * 3, reverse=True)
-    for n in numbers:
-        if answer == '' and n == '0':
-            return '0'
-        answer += n
-    return answer
+    return str(int(''.join(numbers)))
