882752d8352cdebf318c02375ef9bd231f6a4dcb
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2024-02-03 16:24:21 +0900

    [level 1] Title: 모의고사, Time: 4.51 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 4.77 ms
+메모리: 10.1 MB, 시간: 4.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 5일 18:58:52
+2024년 2월 6일 16:24:20
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,20 +1,17 @@
 # 프로그래머스 42840 모의고사 LV.1
 def solution(answers):
-    answer = []
-    one = [1, 2, 3, 4, 5]
-    two = [2, 1, 2, 3, 2, 4, 2, 5]
-    three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    first = [1, 2, 3, 4, 5]
+    second = [2, 1, 2, 3, 2, 4, 2, 5]
+    third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     scores = [0, 0, 0]
 
     for i in range(len(answers)):
-        if answers[i] == one[(i + 1) % (len(one)) - 1]:
+        if answers[i] == first[(i + 1) % (len(first)) - 1]:
             scores[0] += 1
-        if answers[i] == two[(i + 1) % (len(two)) - 1]:
+        if answers[i] == second[(i + 1) % (len(second)) - 1]:
             scores[1] += 1
-        if answers[i] == three[(i + 1) % (len(three)) - 1]:
+        if answers[i] == third[(i + 1) % (len(third)) - 1]:
             scores[2] += 1
     max_score = max(scores)
-    for i in range(len(scores)):
-        if scores[i] == max_score:
-            answer.append(i + 1)
-    return answer
+
+    return [i + 1 for i in range(len(scores)) if max_score == scores[i]]
