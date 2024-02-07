c1ae56658c43623cbcaf17a99f192bb7fd7755f4
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2024-02-07 15:12:37 +0900

    [level 1] Title: 체육복, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,16 @@
+# 프로그래머스 42862 체육복 LV.1
+def solution(n, lost, reserve):
+    lost_set = set(lost)
+    reserve_set = set(reserve)
+    reserve_and_lost = lost_set & reserve_set
+
+    lost_set -= reserve_and_lost
+    reserve_set -= reserve_and_lost
+
+    for i in reserve_set:
+        if i - 1 in lost_set:
+            lost_set -= {i - 1, }
+        elif i + 1 in lost_set:
+            lost_set -= {i + 1, }
+
+    return n - len(lost_set)
