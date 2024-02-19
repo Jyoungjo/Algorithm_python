fe78c4f487b0a3c52f67329377b52d8477cf0e05
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2024-02-19 16:13:25 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,20 +1,13 @@
 # 프로그래머스 42883 큰 수 만들기 LV.2
 def solution(number, k):
-    answer = []
+    answer = [number[0]]
 
-    for n in number:
-        if not answer:
-            answer.append(n)
-            continue
-        while answer[-1] < n and k > 0:
-            answer.pop()
+    for n in number[1:]:
+        while answer and answer[-1] < n and k > 0:
             k -= 1
-            if not answer or k <= 0:
-                break
+            answer.pop()
         answer.append(n)
 
-    if len(answer) != len(number) - k:
-        for _ in range(len(answer) - len(number) + k):
-            answer.pop()
-
+    if k != 0:
+        answer = answer[:-k]
     return ''.join(answer)
