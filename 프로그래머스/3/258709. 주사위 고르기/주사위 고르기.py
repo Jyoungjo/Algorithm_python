f3fc080048ffbc52f39cc4f4b37278b1c9edb795
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2024-03-18 10:39:22 +0900

    [level 3] Title: 주사위 고르기, Time: 6699.48 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -1,3 +1,4 @@
+# 프로그래머스 258709 주사위 고르기 LV.3
 from itertools import combinations, product
 from bisect import bisect_left
 
@@ -7,16 +8,16 @@ def solution(dice):
     total = len(dice)
     for A_dice in combinations(range(total), total // 2):
         B_dice = tuple(i for i in range(total) if i not in A_dice)
-        
+
         A_dice_sum, B_dice_sum = [], []
         for prod in product(range(6), repeat=total // 2):
             A_dice_sum.append(sum(dice[i][j] for i, j in zip(A_dice, prod)))
             B_dice_sum.append(sum(dice[i][j] for i, j in zip(B_dice, prod)))
-        
+
         B_dice_sum.sort()
         wins = sum(bisect_left(B_dice_sum, A) for A in A_dice_sum)
         win_combi[wins] = list(A_dice)
-        
+
     max_key = max(win_combi.keys())
-    
+
     return [x + 1 for x in win_combi[max_key]]
