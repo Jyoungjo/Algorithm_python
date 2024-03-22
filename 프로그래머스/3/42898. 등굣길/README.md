b4eb5cceb8d2e64bc34999e62ccbc68f13fd25e0
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2024-03-23 07:19:18 +0900

    [level 3] Title: 등굣길, Time: 1.90 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 1.97 ms
+메모리: 10.2 MB, 시간: 1.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 13일 06:26:20
+2024년 03월 23일 07:19:16
 
 ### 문제 설명
 
--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,21 +1,21 @@
 # 프로그래머스 42898 등굣길 LV.3
 def solution(m, n, puddles):
-    map = [[1 for _ in range(m)] for _ in range(n)]
+    answer = 0
+    dp = [[1] * m for _ in range(n)]
     denominator = 1000000007
-
-    for x, y in puddles:
-        if x == 1:
-            for i in range(y - 1, n):
-                map[i][x - 1] = 0
+    
+    for y, x in puddles:
         if y == 1:
-            for i in range(x - 1, m):
-                map[y - 1][i] = 0
-        map[y - 1][x - 1] = 0
-
-    for x in range(m):
-        for y in range(n):
-            if x - 1 < 0 or y - 1 < 0 or map[y][x] == 0:
-                continue
-            map[y][x] = map[y][x - 1] + map[y - 1][x]
-
-    return map[n - 1][m - 1] % denominator
+            for i in range(x - 1, n):
+                dp[i][y - 1] = 0
+        if x == 1:
+            for i in range(y - 1, m):
+                dp[x - 1][i] = 0
+        dp[x - 1][y - 1] = 0
+    
+    for x in range(n):
+        for y in range(m):
+            if 0 <= x - 1 <= n and 0 <= y - 1 <= m and dp[x][y] != 0:
+                dp[x][y] = dp[x - 1][y] + dp[x][y - 1]
+                
+    return dp[-1][-1] % denominator
