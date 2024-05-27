389c69e0c749235acb04f221eebbe993dce68d26
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2024-05-27 19:32:31 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -0,0 +1,9 @@
+from math import gcd
+
+
+def solution(arr):
+    while len(arr) >= 2:
+        a, b = arr[0], arr[1]
+        arr.append((a*b) // gcd(a, b))
+        arr = arr[2:]
+    return arr[0]
