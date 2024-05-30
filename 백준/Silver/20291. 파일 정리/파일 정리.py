fb4479320bfd0b550aab2cc8238dd4712a96cb05
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2024-05-30 17:36:15 +0900

    [Silver III] Title: 파일 정리, Time: 176 ms, Memory: 55996 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.py
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -3,14 +3,8 @@
 # 파일의 확장자 별 정리 -> 몇 개 있는지
 # 확장자 사전 순 정렬
 import sys
-from collections import defaultdict
-
+from collections import Counter
 
 N = int(sys.stdin.readline())
-extensions = defaultdict(int)
-for x in range(N):
-    n, e = sys.stdin.readline().split('.')
-    extensions[e[:-1]] += 1
-
-for k, v in sorted(extensions.items()):
-    print(k, v)
+extensions = [sys.stdin.readline().strip().split('.')[1] for _ in range(N)]
+print("\n".join(f"{k} {v}" for k, v in sorted(Counter(extensions).items())))
