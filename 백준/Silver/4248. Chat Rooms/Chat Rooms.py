01ea171bba73ad989a631b022b222907043ab2f8
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2024-05-30 16:22:10 +0900

    [Silver II] Title: Chat Rooms, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4248. Chat Rooms/Chat Rooms.py
+++ 백준/Silver/4248. Chat Rooms/Chat Rooms.py
@@ -1,64 +1,57 @@
-# 1. 한 줄에 5개 이상의 연속된 자음이 포함되면 안 됨
-# 2. 한 줄에 4개 이상의 연속된 자음을 포함하는 단어가 있고, 
-#    사용자가 지난 10개의 매세지 중 이런 줄을 2번 이상 보냈으면 안 됨
-# 3. 사용자가 지난 10개의 메세지 중 동일한 줄을 2번 이상 보냈으면 안 됨
-# -> 각각 함수로 구현
+# 백준 4248 Chat Rooms - 실버 2
 
-# 최근 10줄은 유저가 보냈으나 채팅창에 보내지지 않은 메시지까지 포함
+# 악플 판별 문제
+consonants = 'bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ'
+N = int(input())
+answer = ['y'] * N
+log = [input() for _ in range(N)]
+rule2_arr = [False] * N
 
-# * 반례
-# input().strip()으로 채팅 데이터를 가져왔는데
-# 채팅이
-# "Hello"
-# "Hello "
-# 이런 형태일 경우, 두 채팅을 같은 채팅으로 확인해 오답
 
-CONSONANTS = "bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ"
-
-N = int(input().strip())
-answers = ['y'] * N
-check4Result = [False] * N
-chats = [""] * N
-for i in range(N):
-    chats[i] = input()
-
-def check5(line):
-    count = 0
-    for c in line:
-        if c in CONSONANTS:
-            count += 1
-            if count > 5:
+# 1. 5개 초과의 연속 자음 단어
+def rule1(sentence):
+    c_count = 0
+    for word in sentence:
+        if word in consonants:
+            c_count += 1
+            if c_count > 5:
                 return True
         else:
-            count = 0
+            c_count = 0
     return False
 
-def check4(line, index):
-    words = line.split()
-    count = 0
+
+# 2. 연속 자음 4개 초과인 단어가 하나 이상 포함 + 최근 10줄에서 해당 줄을 3개 이상 보냄
+def rule2(sentence, idx):
+    words = sentence.split()
+    c_count = 0
 
     for word in words:
         for c in word:
-            if c in CONSONANTS:
-                count += 1
-                if count > 4:
-                    check4Result[index] = True
+            if c in consonants:
+                c_count += 1
+                if c_count > 4:
+                    rule2_arr[idx] = True
                     break
             else:
-                count = 0
-    
-    if check4Result[index] and check4Result[max(0, index - 10) : index].count(True) > 2:
-        return True 
+                c_count = 0
+
+    if rule2_arr[idx] and rule2_arr[max(0, idx - 10):idx].count(True) > 2:
+        return True
     else:
         return False
-    
-def checkTwice(line, index):
-    if chats[max(0, index - 10):index].count(line) > 1: return True
-    else: return False
-
-for i, line in enumerate(chats):
-    if check5(line) or check4(line, i) or checkTwice(line, i):
-        answers[i] = 'n'
 
 
-print('\n'.join(answers))
+# 3. 최근 10줄에서 같은 줄 두 번 이상
+def rule3(sentence, idx):
+    if log[max(0, idx - 10):idx].count(sentence) > 1:
+        return True
+    else:
+        return False
+
+
+for i, target in enumerate(log):
+    if rule1(target) or rule2(target, i) or rule3(target, i):
+        answer[i] = 'n'
+
+print('\n'.join(answer))
