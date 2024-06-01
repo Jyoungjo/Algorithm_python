394afa2e811d014c0fdd7c26d764d7489c9d260f
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2024-06-01 12:53:57 +0900

    [Silver I] Title: IOIOI, Time: 380 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,6 +1,8 @@
 # 백준 5525 IOIOI - 실버 1
 
 # Pn = N+1개의 I와 N개의 O로 이루어진 문자열
+# Pn 만드는 과정에서 메모리 초과 -> 문자열 더하기 연산을 append로 해결
+# 이후 시간 초과 -> 슬라이싱으로 비교했는데 여기서 문제?
 import sys
 
 
@@ -8,18 +10,16 @@ N = int(sys.stdin.readline().strip())
 M = int(sys.stdin.readline().strip())
 S = sys.stdin.readline().strip()
 
-# Pn 만들기
-arr = ['I']
-for i in range(N):
-    arr.append('OI')
-Pn = ''.join(arr)
-
-cnt, idx = 0, 0
-# 문자열 S에서 I로 시작하는 부분부터 슬라이싱해서 일치여부 확인
-while idx < M:
-    if S[idx] == 'I' and S[idx:idx+len(Pn)] == Pn:
-        cnt += 1
-        idx += 2
-    else:
+cnt, idx, pattern = 0, 1, 0
+while idx < M - 1:
+    if S[idx - 1] == 'I' and S[idx] == 'O' and S[idx + 1] == 'I':
+        pattern += 1
+        if pattern == N:
+            pattern -= 1
+            cnt += 1
         idx += 1
+    else:
+        pattern = 0
+    idx += 1
+
 print(cnt)
