eccbc0e7d9c5d39067fff276b58b947b130634c7
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2024-06-06 09:37:45 +0900

    [Silver IV] Title: 수 찾기, Time: 112 ms, Memory: 50380 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,17 +1,10 @@
 # 백준 1920 수 찾기 - 실버 4
 
-# 31120/44
 import sys
-from collections import Counter
-
 input = sys.stdin.readline
 
 
 N = int(input())
-A = list(map(int, input().strip().split()))
+A = set(input().strip().split())
 M = int(input())
-B = list(map(int, input().strip().split()))
-
-c = Counter(A)
-for b in B:
-    print(1) if c[b] else print(0)
+print('\n'.join('1' if num in A else '0' for num in input().strip().split()))
