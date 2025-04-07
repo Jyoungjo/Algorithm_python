61e3348301ea840b49e7d5829d502b5bd6f105d7
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-04-07 13:50:52 +0900

    [level 2] Title: [3차] n진수 게임, Time: 15.12 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -3,8 +3,9 @@ import java.util.*;
 class Solution {
     public String solution(int n, int t, int m, int p) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i <= 100000; i++) {
-            sb.append(convert(i, n));
+        int num = 0;
+        while (sb.length() <= m * t) {
+            sb.append(Integer.toString(num++, n));
         }
         
         String target = sb.toString();
@@ -14,36 +15,6 @@ class Solution {
             sb.append(target.charAt(i));
         }
         
-        return sb.toString();
-    }
-    
-    private String convert(int num, int n) {
-        if (num == 0) return String.valueOf(0);
-        
-        StringBuilder sb = new StringBuilder();
-        while (num != 0) {
-            sb.append(divide(num % n));
-            num /= n;
-        }
-        return sb.reverse().toString();
-    }
-    
-    private String divide(int num) {
-        switch (num) {
-            case 10:
-                return "A";
-            case 11:
-                return "B";
-            case 12:
-                return "C";
-            case 13:
-                return "D";
-            case 14:
-                return "E";
-            case 15:
-                return "F";
-            default:
-                return String.valueOf(num);
-        }
+        return sb.toString().toUpperCase();
     }
 }
