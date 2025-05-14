e98620ee1d95f3dec1397b263413f2b9b1e8c528
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-05-14 17:39:03 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 48.69 ms, Memory: 211 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,36 +1,44 @@
-import java.util.*;
-
 class Solution {
+    int R, C;
+    final int ENEMY = 1, OUR = 2;
+    
     public int solution(int[][] board, int[][] skill) {
-        int answer = 0, r = board.length, c = board[0].length;
-        int[][] arr = new int[r + 1][c + 1];
+        int answer = 0;
+        R = board.length; C = board[0].length;
+        int[][] damaged = new int[R + 1][C + 1];
         
-        // skill 반복문 돌면서 각 좌표 범위 지정 -> O(skill.length) = 250000
         for (int[] s : skill) {
-            int r1 = s[1], c1 = s[2], r2 = s[3], c2 = s[4], val = s[0] == 1 ? -s[5] : s[5];
-            arr[r1][c1] += val;
-            arr[r1][c2 + 1] -= val;
-            arr[r2 + 1][c1] -= val;
-            arr[r2 + 1][c2 + 1] += val;
+            int type = s[0], r1 = s[1], c1 = s[2], r2 = s[3], c2 = s[4], degree = s[5];
+            
+            if (type == ENEMY) {
+                recordDamage(damaged, r1, c1, r2, c2, degree);
+                continue;
+            }
+            
+            recordDamage(damaged, r1, c1, r2, c2, -degree);
         }
         
-        // 누적합 계산을 통한 전체 범위 변화량 계산 -> O(r * c) = 1000000
-        for (int i = 0; i <= r; i++) {
-            for (int j = 0; j <= c; j++) {
-                if (i > 0) arr[i][j] += arr[i - 1][j];
-                if (j > 0) arr[i][j] += arr[i][j - 1];
-                if (i > 0 && j > 0) arr[i][j] -= arr[i - 1][j - 1];
+        for (int i = 0; i <= R; i++) {
+            for (int j = 0; j <= C; j++) {
+                if (i > 0) damaged[i][j] += damaged[i - 1][j];
+                if (j > 0) damaged[i][j] += damaged[i][j - 1];
+                if (i > 0 && j > 0) damaged[i][j] -= damaged[i - 1][j - 1];
             }
         }
         
-        // board의 각 위치에 해당하는 값 적용 -> O(r * c) = 1000000
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                board[i][j] += arr[i][j];
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                board[i][j] -= damaged[i][j];
                 if (board[i][j] > 0) answer++;
             }
         }
-        
         return answer;
+    }
+    
+    private void recordDamage(int[][] damaged, int r1, int c1, int r2, int c2, int degree) {
+        damaged[r1][c1] += degree;
+        damaged[r1][c2 + 1] += -degree;
+        damaged[r2 + 1][c1] += -degree;
+        damaged[r2 + 1][c2 + 1] += degree;
     }
 }
