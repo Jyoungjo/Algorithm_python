a1199253b4d7fa6fb3a9c384905037c46b0e4fee
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-05-15 16:42:10 +0900

    [level 3] Title: 양과 늑대, Time: 35.17 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/README.md
+++ 프로그래머스/3/92343. 양과 늑대/README.md
@@ -1,10 +1,10 @@
 # [level 3] 양과 늑대 - 92343 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92343#qna) 
 
 ### 성능 요약
 
-메모리: 97.6 MB, 시간: 37.00 ms
+메모리: 105 MB, 시간: 35.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 26일 11:15:19
+2025년 05월 15일 16:42:07
 
 ### 문제 설명
 
--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,52 +1,42 @@
 import java.util.*;
 
 class Solution {
-    int[][] tree;
-    int[] arr;
-    int v, answer = 0;
+    final int ROOT_NODE = 0;
+    
+    List<Integer>[] graph;
+    int N;
+    int answer = Integer.MIN_VALUE;
     
     public int solution(int[] info, int[][] edges) {
         init(info, edges);
-        Set<Integer> set = new HashSet<>();
-        set.add(0);
-        dfs(1, 0, set);
+        Set<Integer> destinations = new HashSet<>(graph[ROOT_NODE]);
+        backtracking(info, destinations, ROOT_NODE, 1, 0);
         return answer;
     }
     
-    private void dfs(int sheep, int wolf, Set<Integer> set) {
-        if (sheep <= wolf) return;
-        answer = Math.max(answer, sheep);
-        
-        for (int now : set) {
-            if (tree[now][0] != -1 && !set.contains(tree[now][0])) {
-                Set<Integer> tmpSet = new HashSet<>(set);
-                tmpSet.add(tree[now][0]);
-                if (arr[tree[now][0]] == 0) dfs(sheep + 1, wolf, tmpSet);
-                else dfs(sheep, wolf + 1, tmpSet);
-            }
-            
-            if (tree[now][1] != -1 && !set.contains(tree[now][1])) {
-                Set<Integer> tmpSet = new HashSet<>(set);
-                tmpSet.add(tree[now][1]);
-                if (arr[tree[now][1]] == 0) dfs(sheep + 1, wolf, tmpSet);
-                else dfs(sheep, wolf + 1, tmpSet);
-            }
+    private void init(int[] info, int[][] edges) {
+        N = info.length;
+        graph = new List[N + 1];
+        for (int i = 0; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int[] edge : edges) {
+            graph[edge[0]].add(edge[1]);
         }
     }
     
-    private void init(int[] info, int[][] edges) {
-        v = info.length;
-        tree = new int[v][2];
-        arr = info;
+    private void backtracking(int[] info, Set<Integer> destinations, int curNode, int sheep, int wolf) {
+        if (sheep <= wolf) return;
         
-        for (int i = 0; i < v; i++) {
-            Arrays.fill(tree[i], -1);
-        }
+        answer = Math.max(answer, sheep);
         
-        for (int[] edge : edges) {
-            int s = edge[0], e = edge[1];
-            if (tree[s][0] == -1) tree[s][0] = e;
-            else tree[s][1] = e;
+        for (int next : destinations) {
+            Set<Integer> nextDest = new HashSet<>(destinations);
+            nextDest.remove(next);
+            nextDest.addAll(graph[next]);
+
+            if (info[next] == 0) backtracking(info, nextDest, next, sheep + 1, wolf);
+            else backtracking(info, nextDest, next, sheep, wolf + 1);
         }
     }
 }
