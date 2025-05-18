d1cf67d629920f346bb8dd9742db87b2d9ab69da
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-05-18 17:14:12 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 60.63 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,39 +1,32 @@
 import java.util.*;
 
 class Solution {
-    Map<String, Integer> result = new HashMap<>();
-    Map<String, String> map = new HashMap<>();
+    Map<String, Integer> marginMap = new HashMap<>();
+    Map<String, String> referralMap = new HashMap<>();
     
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
         int[] answer = new int[enroll.length];
-        init(enroll, referral);
+        for (int i = 0; i < enroll.length; i++) {
+            marginMap.put(enroll[i], 0);
+            referralMap.put(enroll[i], referral[i]);
+        }
+        
         for (int i = 0; i < seller.length; i++) {
             dfs(seller[i], amount[i] * 100);
         }
+        
         for (int i = 0; i < enroll.length; i++) {
-            answer[i] = result.get(enroll[i]);
+            answer[i] = marginMap.get(enroll[i]);
         }
+        
         return answer;
     }
     
-    private void dfs(String sel, int amo) {
-        /*
-            수수료 = 현재 비용 * 0.1
-            수익 = 비용 - 수수료
-        */
-        String parent = map.get(sel);
-        int fee = (int) (amo * 0.1);
-        int revenue = amo - fee;
+    private void dfs(String seller, int profit) {  
+        String ref = referralMap.get(seller);
+        int yours = (int) (profit * 0.1);
+        marginMap.put(seller, marginMap.get(seller) + profit - yours);
         
-        // System.out.printf("seller: %s, val: %d\n", sel, revenue);
-        result.put(sel, result.get(sel) + revenue);
-        if (!parent.equals("-") && amo >= 10) dfs(parent, fee);
-    }
-    
-    private void init(String[] enroll, String[] referral) {
-        for (int i = 0; i < enroll.length; i++) {
-            result.put(enroll[i], 0);
-            map.put(enroll[i], referral[i]);
-        }
+        if (!ref.equals("-") && profit >= 10) dfs(referralMap.get(seller), yours);
     }
 }
