249e282185423217266c7fb07da811664c9460ea
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-05-19 15:20:39 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 90.08 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,19 +1,18 @@
+import java.util.*;
+import java.util.stream.*;
+
 class Solution {
     public long[] solution(long[] numbers) {
-        long[] answer = new long[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            long x = numbers[i];
-            String target = Long.toBinaryString(x);
-            
-            if (x % 2 == 0) {
-                answer[i] = x + 1;
-                continue;
-            } else {
-                int idx = target.lastIndexOf("0");
-                if (idx == -1) answer[i] = Long.parseLong("10" + target.substring(1), 2);
-                else answer[i] = Long.parseLong(target.substring(0, idx) + "10" + target.substring(idx + 2), 2);
+        List<Long> answer = new ArrayList<>();
+        for (long num : numbers) {
+            String bit = Long.toBinaryString(num);
+            if (num % 2 == 0) answer.add(Long.parseLong(bit.substring(0, bit.length() - 1) + "1", 2));
+            else {
+                int idx = bit.lastIndexOf("0");
+                if (idx == -1) answer.add(Long.parseLong("10" + bit.substring(1), 2));
+                else answer.add(Long.parseLong(bit.substring(0, idx) + "10" + bit.substring(idx + 2), 2));
             }
         }
-        return answer;
+        return answer.stream().mapToLong(Long::longValue).toArray();
     }
 }
