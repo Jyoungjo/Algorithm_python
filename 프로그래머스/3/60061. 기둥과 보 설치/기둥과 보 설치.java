9b595e3a8a8bafa2526e002f07505e0f41e08964
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-05-28 17:38:13 +0900

    [level 3] Title: 기둥과 보 설치, Time: 24.49 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.java
+++ 프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.java
@@ -1,104 +1,78 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     boolean[][][] frame;
     int N;
-    int cnt = 0;
     
     public int[][] solution(int n, int[][] build_frame) {
-        N = n;
         frame = new boolean[n + 1][n + 1][2];
-        build(build_frame);
-        int[][] answer = new int[cnt][3];
-        int idx = 0;
+        N = n;
+        
+        List<int[]> answer = new ArrayList<>();
+        for (int[] bf : build_frame) {
+            int x = bf[0], y = bf[1], type = bf[2], install = bf[3];
+            if (install == 1) {
+                if (canInstall(x, y, type)) frame[y][x][type] = true;
+            } else {
+                frame[y][x][type] = false;
+                if (!canRemove(x, y, type)) frame[y][x][type] = true;
+            }
+        }
+        
         for (int i = 0; i <= N; i++) {
-            for (int j = N; j >= 0; j--) {
+            for (int j = 0; j <= N; j++) {
                 for (int k = 0; k < 2; k++) {
-                    if (frame[i][j][k]) {
-                        answer[idx][0] = i;
-                        answer[idx][1] = N - j;
-                        answer[idx++][2] = k;
-                    }
+                    if (frame[i][j][k]) answer.add(new int[]{j, i, k});
                 }
             }
         }
-        return answer;
+        
+        answer.sort((a, b) -> {
+            if (a[0] == b[0] && a[1] == b[1]) return a[2] - b[2];
+            if (a[0] == b[0]) return a[1] - b[1];
+            return a[0] - b[0];
+        });
+        
+        return answer.toArray(int[][]::new);
     }
     
-    private void build(int[][] build_frame) {
-        for (int[] info : build_frame) {
-            /*
-                설치 조건
-                기둥 - 바닥 위, 보 끝 부분, 다른 기둥 위
-                보 - 기둥 위, 양쪽 끝이 다른 보와 연결
-
-                a - 구조물 종류 (0 = 기둥, 1 = 보)
-                b - 구조물 설치 및 삭제 (0 = 삭제, 1 = 설치)
-                1 = 기둥, 2 = 보
-            */
-            int x = info[0], y = N - info[1], a = info[2], b = info[3];
-            // 설치
-            if (b == 1) {
-                if (isInstallable(x, y, a)) {
-                    frame[x][y][a] = true;
-                    cnt++;
-                }
-            }
-            // 삭제
-            else {
-                frame[x][y][a] = false;
-                if (isRemovable(x, y, a)) cnt--;
-                else frame[x][y][a] = true;
-            }
+    private boolean canInstall(int x, int y, int type) {
+        if (type == 0) { // 기둥
+            // 바닥 위에 있으면 설치 가능
+            if (y == 0) return true;
+            // 보의 한쪽 끝 부분 위에 있으면 설치 가능
+            if (frame[y][x][1] || (x > 0 && frame[y][x - 1][1])) return true;
+            // 다른 기둥 위에 있으면 설치 가능
+            if (y > 0 && frame[y - 1][x][0]) return true;
+        } else { // 보
+            // 한쪽 끝 부분이 기둥 위에 있으면 설치 가능
+            if ((y > 0 && frame[y - 1][x][0]) || (y > 0 && x < N && frame[y - 1][x + 1][0])) return true;
+            // 양쪽 끝 부분이 다른 보와 동시에 연결되어 있으면 설치 가능
+            if ((x > 0 && frame[y][x - 1][1]) && (x < N && frame[y][x + 1][1])) return true;
         }
-    }
-    
-    private boolean isRemovable(int x, int y, int a) {
-        // 기둥
-        if (a == 0) {
-            if (
-                (x - 1 >= 0 && frame[x - 1][y - 1][1] && !isInstallable(x - 1, y - 1, 1)) ||
-                (x + 1 >= 0 && frame[x][y - 1][1] && !isInstallable(x, y - 1, 1)) ||
-                (y - 1 >= 0 && frame[x][y - 1][0] && !isInstallable(x, y - 1, 0))
-            ) {
-                return false;
-            }
-        }
-        // 보
-        else {
-            if (
-                (y - 1 >= 0 && frame[x][y][0] && !isInstallable(x, y, 0)) ||
-                (y - 1 >= 0 && frame[x + 1][y][0] && !isInstallable(x + 1, y, 0)) ||
-                (x - 1 >= 0 && frame[x - 1][y][1] && !isInstallable(x - 1, y, 1)) ||
-                (x + 2 <= N && frame[x + 1][y][1] && !isInstallable(x + 1, y, 1))
-            ) {
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    private boolean isInstallable(int x, int y, int a) {
-        // 기둥
-        if (a == 0) {
-            // 바닥 위, 보 끝 부분, 다른 기둥 위
-            if (y == N || (x - 1 >= 0 && frame[x - 1][y][1]) ||
-                (x + 1 <= N && frame[x][y][1]) || (y + 1 <= N && frame[x][y + 1][0])) 
-            {
-                return true;
-            }
-        }
-        // 보
-        else {
-            // 기둥 위, 양쪽 끝이 다른 보와 연결
-            if (
-                (y + 1 <= N && frame[x][y + 1][0]) || (y + 1 <= N && frame[x + 1][y + 1][0]) ||
-                (x - 1 >= 0 && x + 2 <= N && frame[x - 1][y][1] && frame[x + 1][y][1])) 
-            {
-                return true;
-            }
-        }
+        
         return false;
+    }
+    
+    private boolean canRemove(int x, int y, int type) {
+        if (type == 0) { // 기둥
+            // 해당 기둥 바로 위에 보가 존재할 경우 그 보가 설치 안 되면 없앨 수 없음
+            if (y < N && frame[y + 1][x][1] && !canInstall(x, y + 1, 1)) return false;
+            // 해당 기둥 왼쪽 위에 보가 존재할 경우 그 보가 설치 안 되면 없앨 수 없음
+            if (y < N && x > 0 && frame[y + 1][x - 1][1] && !canInstall(x - 1, y + 1, 1)) return false;
+            // 해당 기둥 위에 다른 기둥이 존재할 경우 그 기둥이 설치 안 되면 없앨 수 없음
+            if (y < N && frame[y + 1][x][0] && !canInstall(x, y + 1, 0)) return false;
+        } else { // 보
+            // 해당 보 위치에 기둥이 존재할 경우 그 기둥이 설치 안 되면 없앨 수 없음
+            if (frame[y][x][0] && !canInstall(x, y, 0)) return false;
+            // 해당 보 오른쪽에 기둥이 존재할 경우 그 기둥이 설치 안 되면 없앨 수 없음
+            if (x < N && frame[y][x + 1][0] && !canInstall(x + 1, y, 0)) return false;
+            // 해당 보 왼쪽에 보가 존재할 경우 그 보가 설치 안 되면 없앨 수 없음
+            if (x > 0 && frame[y][x - 1][1] && !canInstall(x - 1, y, 1)) return false;
+            // 해당 보 오른쪽에 보가 존재할 경우 그 보가 설치 안 되면 없앨 수 없음
+            if (x < N && frame[y][x + 1][1] && !canInstall(x + 1, y, 1)) return false;
+        }
+        
+        return true;
     }
 }
