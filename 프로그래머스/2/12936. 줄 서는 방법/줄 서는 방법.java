9f5192acb589ac77ac94fea979e4f3ec12ac6830
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-06-09 17:07:52 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.06 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -1,25 +1,25 @@
 import java.util.*;
 
 class Solution {
-    
     public int[] solution(int n, long k) {
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 1; i <= n; i++) numbers.add(i);
-
-        long[] factorial = new long[n + 1];
-        factorial[0] = 1;
-        for (int i = 1; i <= n; i++) factorial[i] = factorial[i - 1] * i;
-
-        int[] result = new int[n];
-        k--;
-
-        for (int i = 0; i < n; i++) {
-            int idx = (int)(k / factorial[n - 1 - i]);
-            result[i] = numbers.get(idx);
-            numbers.remove(idx);
-            k %= factorial[n - 1 - i];
+        int[] answer = new int[n];
+        long[] dp = new long[n + 1];
+        List<Integer> nums = new ArrayList<>();
+        
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            nums.add(i);
+            dp[i] = dp[i - 1] * i;
         }
-
-        return result;
+        
+        k--;
+        for (int i = 0; i < n; i++) {
+            int idx = (int) (k / dp[n - 1 - i]);
+            answer[i] = nums.get(idx);
+            nums.remove(idx);
+            k %= dp[n - 1 - i];
+        }
+        
+        return answer;
     }
 }
