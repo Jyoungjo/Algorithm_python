d484d87c3a6fbf98dfd2ee743054f23690c56790
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-06-18 16:25:14 +0900

    [level 3] Title: 캠핑, Time: 2574.86 ms, Memory: 398 MB -BaekjoonHub

--- 프로그래머스/3/1833. 캠핑/캠핑.java
+++ 프로그래머스/3/1833. 캠핑/캠핑.java
@@ -2,14 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] data) {
-        int answer = 0;
-        
         List<Integer> xList = new ArrayList<>();
         List<Integer> yList = new ArrayList<>();
         
-        for (int[] p : data) {
-            xList.add(p[0]);
-            yList.add(p[1]);
+        for (int[] d : data) {
+            xList.add(d[0]);
+            yList.add(d[1]);
         }
         
         List<Integer> uniqueXList = new ArrayList<>(new HashSet<>(xList));
@@ -26,19 +24,15 @@ class Solution {
             dp[data[i][1]][data[i][0]] = 1;
         }
         
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < n; c++) {
-                if (r - 1 >= 0) dp[r][c] += dp[r - 1][c];
-                if (c - 1 >= 0) dp[r][c] += dp[r][c - 1];
-                if (r - 1 >= 0 && c - 1 >= 0) dp[r][c] -= dp[r - 1][c - 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i >= 1) dp[i][j] += dp[i - 1][j];
+                if (j >= 1) dp[i][j] += dp[i][j - 1];
+                if (i >= 1 && j >= 1) dp[i][j] -= dp[i - 1][j - 1];
             }
         }
         
-        Arrays.sort(data, (o1, o2) -> {
-            if (o1[0] == o2[0]) return o1[1] - o2[1];
-            return o1[0] - o2[0];
-        });
-        
+        int answer = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 int r1 = Math.min(data[i][1], data[j][1]);
@@ -48,9 +42,8 @@ class Solution {
                 
                 if (r1 == r2 || c1 == c2) continue;
                 
-                int cnt = dp[r2 - 1][c2 - 1] - dp[r2 - 1][c1] - dp[r1][c2 - 1] + dp[r1][c1];
-                
-                if (cnt == 0) answer++;
+                int blank = dp[r2 - 1][c2 - 1] - dp[r2 - 1][c1] - dp[r1][c2 - 1] + dp[r1][c1];
+                if (blank == 0) answer++;
             }
         }
         
