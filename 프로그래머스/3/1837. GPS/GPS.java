828c21ac3cf0c752c6a81b2e6c9fe67bc1f87015
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-06-20 16:14:58 +0900

    [level 3] Title: GPS, Time: 42.33 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/3/1837. GPS/GPS.java
+++ 프로그래머스/3/1837. GPS/GPS.java
@@ -1,41 +1,34 @@
 import java.util.*;
 
 class Solution {
-    List<Integer>[] graph;
-    
     public int solution(int n, int m, int[][] edge_list, int k, int[] gps_log) {
-        makeGraph(n, edge_list);
+        List<Integer>[] graph = new List[n + 1];
+        for (int i = 0; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int[] edge : edge_list) {
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
+        }
+        
         int[][] dp = new int[k][n + 1];
         for (int i = 0; i < k; i++) {
             Arrays.fill(dp[i], 987654321);
         }
-        dp[0][gps_log[0]] = 0;
         
+        dp[0][gps_log[0]] = 0;
         for (int i = 1; i < k; i++) {
             for (int j = 1; j <= n; j++) {
                 int prev = dp[i - 1][j];
-                int goal = gps_log[i];
+                int val = gps_log[i];
                 
                 for (int next : graph[j]) {
-                    if (next == goal) dp[i][next] = Math.min(dp[i][next], prev);
+                    if (next == val) dp[i][next] = Math.min(dp[i][next], prev);
                     else dp[i][next] = Math.min(dp[i][next], prev + 1);
                 }
             }
         }
         
-        int end = gps_log[gps_log.length - 1];
-        return dp[k - 1][end] == 987654321 ? -1 : dp[k - 1][end];
-    }
-    
-    private void makeGraph(int n, int[][] edge_list) {
-        graph = new List[n + 1];
-        for (int i = 0; i <= n; i++) {
-            graph[i] = new ArrayList<>();
-        }
-        
-        for (int[] edge : edge_list) {
-            graph[edge[0]].add(edge[1]);
-            graph[edge[1]].add(edge[0]);
-        }
+        return dp[k - 1][gps_log[gps_log.length - 1]] == 987654321 ? -1 : dp[k - 1][gps_log[gps_log.length - 1]];
     }
 }
--- 프로그래머스/3/1837. GPS/README.md
+++ 프로그래머스/3/1837. GPS/README.md
@@ -1,10 +1,10 @@
 # [level 3] GPS - 1837 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1837#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1837) 
 
 ### 성능 요약
 
-메모리: 103 MB, 시간: 43.49 ms
+메모리: 113 MB, 시간: 42.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 09일 16:31:57
+2025년 06월 20일 16:14:56
 
 ### 문제 설명
 
