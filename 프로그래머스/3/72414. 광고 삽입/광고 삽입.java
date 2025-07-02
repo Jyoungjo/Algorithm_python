77cdba25851c29617a8d160e480d2abfc4ba4edc
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-07-02 16:25:08 +0900

    [level 3] Title: 광고 삽입, Time: 3536.29 ms, Memory: 320 MB -BaekjoonHub

--- 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -1,46 +1,51 @@
-import java.util.*;
-
 class Solution {
     public String solution(String play_time, String adv_time, String[] logs) {
-        int play = convertToSec(play_time), adv = convertToSec(adv_time);
-        int[] times = new int[360000];
+        // 누적 재생시간이 가장 많이 나오는 곳에 광고 삽입
+        // return 광고 시작 시각 -> 누적 재생시간 많은 곳 여러개면 가장 빠른 시작 시각
+        int pt = convertToSec(play_time);
+        int at = convertToSec(adv_time);
+        int[] video = new int[pt + 1];
+        
         for (String log : logs) {
-            String[] splited = log.split("-");
-            int s = convertToSec(splited[0]), e = convertToSec(splited[1]);
-            for (int i = s; i < e; i++) {
-                times[i]++;
-            }
+            String[] l = log.split("-");
+            int s = convertToSec(l[0]), e = convertToSec(l[1]);
+            for (int i = s; i < e; i++) video[i]++;
         }
         
         long tot = 0;
-        int idx = 0;
-        for (int i = 0; i < adv; i++) {
-            tot += times[i];
-        }
+        for (int i = 0; i < at; i++) tot += video[i];
         
-        long maxTot = tot;
-        for (int i = adv; i <= play; i++) {
-            tot += times[i] - times[i - adv];
-            if (maxTot < tot) {
-                maxTot = tot;
-                idx = i - adv + 1;
+        long max = tot;
+        int start_time = 0;
+        for (int i = at; i <= pt; i++) {
+            tot += video[i];
+            tot -= video[i - at];
+            
+            if (max < tot) {
+                max = tot;
+                start_time = i - at + 1;
             }
         }
         
-        return convertToStringTime(idx);
+        return convertToString(start_time);
+    }
+    
+    private String convertToString(int start_time) {
+        int h = start_time / 3600;
+        int m = (start_time % 3600) / 60;
+        int s = (start_time % 3600) % 60;
+        return addZero(h) + h + ":" + addZero(m) + m + ":" + addZero(s) + s;
     }
     
     private String addZero(int time) {
         return time < 10 ? "0" : "";
     }
     
-    private String convertToStringTime(int time) {
-        int h = time / 3600, m = (time - h * 3600) / 60, s = time - 3600 * h - 60 * m;
-        return addZero(h) + h + ":" + addZero(m) + m + ":" + addZero(s) + s;
-    }
-    
     private int convertToSec(String time) {
-        int[] tmp = Arrays.stream(time.split(":")).mapToInt(Integer::parseInt).toArray();
-        return 3600 * tmp[0] + 60 * tmp[1] + tmp[2];
+        String[] t = time.split(":");
+        int h = Integer.parseInt(t[0]) * 3600;
+        int m = Integer.parseInt(t[1]) * 60;
+        int s = Integer.parseInt(t[2]);
+        return h + m + s;
     }
 }
