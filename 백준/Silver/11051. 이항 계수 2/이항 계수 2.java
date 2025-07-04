0c5c0084baccfb92fac2276462c5f03011d43731
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-07-04 17:36:08 +0900

    [Silver II] Title: 이항 계수 2, Time: 112 ms, Memory: 17996 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()), K = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[N + 1][N + 1];
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) dp[i][j] = 1;
+                else dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % 10007;
+            }
+        }
+
+        System.out.println(dp[N][K] % 10007);
+    }
+}
