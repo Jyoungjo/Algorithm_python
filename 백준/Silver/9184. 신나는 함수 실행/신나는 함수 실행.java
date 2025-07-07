d9203f7c668a849b0abdcd2e13de6f1bf858b3c6
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-07-07 18:10:03 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 2260 ms, Memory: 104544 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -6,29 +6,29 @@ import java.util.StringTokenizer;
 
 class Main {
     static int[][][] dp;
-    static final int MAX = 987654321;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        dp = new int[102][102][102];
-        for (int i = 0; i < 102; i++) {
-            for (int j = 0; j < 102; j++) Arrays.fill(dp[i][j], MAX);
-        }
+        dp = new int[21][21][21];
         while (true) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken()), c = Integer.parseInt(st.nextToken());
             if (a == -1 && b == -1 && c == -1) break;
-            System.out.printf("w(%d, %d, %d) = %d\n", a, b, c, w(a + 50, b + 50, c + 50));
+            System.out.printf("w(%d, %d, %d) = %d\n", a, b, c, w(a, b, c));
         }
     }
 
     private static int w(int a, int b, int c) {
-        if (dp[a][b][c] != MAX) return dp[a][b][c];
-        if (a <= 50 || b <= 50 || c <= 50) return dp[a][b][c] = 1;
+        if (inRange(a, b, c) && dp[a][b][c] != 0) return dp[a][b][c];
+        if (a <= 0 || b <= 0 || c <= 0) return 1;
 
-        if (a > 70 || b > 70 || c > 70) return dp[a][b][c] = w(70, 70, 70);
+        if (a > 20 || b > 20 || c > 20) return dp[20][20][20] = w(20, 20, 20);
         if (a < b && b < c) return dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
 
         return dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+    }
+
+    private static boolean inRange(int a, int b, int c) {
+        return 0 <= a && a <= 20 && 0 <= b && b <= 20 && 0 <= c && c <= 20;
     }
 }
