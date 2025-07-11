96373db08224114517834d39b2012abee1a137f0
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-07-11 16:15:09 +0900

    [level 3] Title: 매칭 점수, Time: 12.02 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/3/42893. 매칭 점수/매칭 점수.java
+++ 프로그래머스/3/42893. 매칭 점수/매칭 점수.java
@@ -1,82 +1,95 @@
 import java.util.*;
+import java.util.stream.*;
 
-class Web {
+class WebPage {
     int idx;
+    String url;
     String[] ext_urls;
     double score, link_score;
     
-    public Web(int idx) {
+    public WebPage(int idx, double score, String url, String[] ext_urls) {
         this.idx = idx;
-    }
-    
-    public void addExtUrls(String[] htmls) {
-        List<String> result = new ArrayList<>();
-        Arrays.stream(htmls).forEach(h -> result.add(h.split("\"")[0]));
-        this.ext_urls = result.toArray(String[]::new);
-    }
-    
-    public void addScore(double score) {
         this.score = score;
+        this.url = url;
+        this.ext_urls = ext_urls;
     }
 }
 
 class Solution {
-    Map<String, Web> result = new HashMap<>();
-    
     public int solution(String word, String[] pages) {
+        Map<String, WebPage> map = new HashMap<>();
+        
         for (int i = 0; i < pages.length; i++) {
-            String now = pages[i];
-            // URL 등록
-            String url = now.split("<meta property=\"og:url\" content=\"https://")[1].split("\"")[0];
-            Web web = new Web(i);
-            result.put(url, web);
-            // 외부 링크 등록
-            String[] splited = now.split("<a href=\"https://");
-            web.addExtUrls(Arrays.copyOfRange(splited, 1, splited.length));
-            // 기본 점수 계산
-            String[] tmp = now.split("<body>")[1].split("<a href=\"https://");
-            double score = 0;
-            for (String s : tmp) {
-                for (int j = 0; j <= s.length() - word.length(); j++) {
-                    if (isMatch(s, word, j, s.length())) score += 1;
+            String page = pages[i];
+            
+            // 해당 페이지 url 찾기
+            String url = findUrl(page);
+            
+            // 해당 페이지 기본 점수 찾기
+            String body = page.split("<body>")[1].split("</body>")[0].toLowerCase();
+            double score = calScore(body, word.toLowerCase());
+            
+            // 해당 페이지 외부 링크 찾기
+            String[] ext_urls = findExtUrls(page);
+            
+            WebPage web_page = new WebPage(i, score, url, ext_urls);
+            map.put(url, web_page);
+        }
+        
+        for (String url : map.keySet()) {
+            WebPage page = map.get(url);
+            for (String ext : page.ext_urls) {
+                if (map.containsKey(ext)) {
+                    WebPage extPage = map.get(ext);
+                    extPage.link_score += (page.score / page.ext_urls.length);
                 }
             }
-            web.addScore(score);
         }
         
-        // 링크 점수 계산
-        for (String key : result.keySet()) {
-            Web web = result.get(key);
-            String[] ext = web.ext_urls;
-            for (String e : ext) {
-                Web otherWeb = result.get(e);
-                if (otherWeb == null) continue;
-                otherWeb.link_score += (web.score / ext.length);
-            }
-        }
-        
-        // 매칭 점수 계산
-        int answer = -1; double match_score = -1;
-        for (String key : result.keySet()) {
-            Web web = result.get(key);
-            double m_score = web.score + web.link_score;
-            if (match_score < m_score) {
-                match_score = m_score;
-                answer = web.idx;
+        int answer = -1;
+        double max = -1;
+        for (String url : map.keySet()) {
+            WebPage page = map.get(url);
+            if (max < page.score + page.link_score) {
+                max = page.score + page.link_score;
+                answer = page.idx;
+            } else if (max == page.score + page.link_score && answer > page.idx) {
+                answer = page.idx;
             }
         }
         
         return answer;
     }
     
-    private boolean isMatch(String s, String word, int i, int len) {
-        String target = s.substring(i, i + word.length()).toLowerCase();
-        if (!target.equals(word.toLowerCase())) return false;
-        // 앞 글자 확인
-        if (i > 0 && Character.isLetter(s.charAt(i - 1))) return false;
-        // 뒤 글자 확인
-        if (i + word.length() < len && Character.isLetter(s.charAt(i + word.length()))) return false;
+    private String findUrl(String page) {
+        return page.split("<meta property=\"og:url\" content=\"")[1].split("\"")[0];
+    }
+    
+    private double calScore(String body, String word) {
+        double score = 0;
+        int wIdx = 0;
+        for (int j = 0; j < body.length(); j++) {
+            if (wIdx == word.length()) {
+                if ('a' > body.charAt(j) || body.charAt(j) > 'z') score++;
+                wIdx = 0;
+                continue;
+            }
+                
+            if (wIdx < word.length()) {
+                if (body.charAt(j) == word.charAt(wIdx)) wIdx++;
+                else wIdx = 0;
+            }
+        }
         
-        return true;
+        return score;
+    }
+    
+    private String[] findExtUrls(String page) {
+        List<String> ext_urls = new ArrayList<>();
+        
+        String[] exts = page.split("<a href=\"");
+        for (int j = 1; j < exts.length; j++) ext_urls.add(exts[j].split("\"")[0]);
+        
+        return ext_urls.stream().toArray(String[]::new);
     }
 }
