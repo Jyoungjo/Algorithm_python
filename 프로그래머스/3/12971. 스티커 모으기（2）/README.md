54340d11ae46b82175175bcc7961cfd9955dfd73
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-07-15 16:44:24 +0900

    [level 3] Title: 스티커 모으기(2), Time: 8.81 ms, Memory: 59.8 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/README.md
+++ 프로그래머스/3/12971. 스티커 모으기（2）/README.md
@@ -1,10 +1,10 @@
 # [level 3] 스티커 모으기(2) - 12971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12971#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12971) 
 
 ### 성능 요약
 
-메모리: 58.1 MB, 시간: 8.84 ms
+메모리: 59.8 MB, 시간: 8.81 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 15일 11:20:58
+2025년 07월 15일 16:44:22
 
 ### 문제 설명
 
--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -1,16 +1,21 @@
 class Solution {
     public int solution(int sticker[]) {
         int answer = 0;
-        if (sticker.length == 1) return sticker[0];
-        for (int i = 0; i <= 1; i++) {
-            int[] dp = new int[sticker.length - 1];
+        int len = sticker.length;
+        if (len == 1) return sticker[0];
+        
+        for (int i = 0; i < 2; i++) {
+            int[] dp = new int[len - 1];
             dp[0] = sticker[i];
-            for (int j = 1; j < dp.length; j++) {
-                if (j == 1) dp[j] = Math.max(sticker[i + j], dp[j - 1]);
+            
+            for (int j = 1; j < len - 1; j++) {
+                if (j == 1) dp[j] = Math.max(dp[j - 1], sticker[i + j]);
                 else dp[j] = Math.max(dp[j - 1], dp[j - 2] + sticker[i + j]);
             }
-            answer = Math.max(answer, dp[dp.length - 1]);
+            
+            answer = Math.max(answer, dp[len - 2]);
         }
+        
         return answer;
     }
 }
