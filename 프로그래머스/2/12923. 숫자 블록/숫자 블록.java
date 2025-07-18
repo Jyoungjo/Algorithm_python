4e3fd3ceef39b62e198e670ec5b2782bf2dd0629
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-07-18 16:27:46 +0900

    [level 2] Title: 숫자 블록, Time: 331.92 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
@@ -2,29 +2,25 @@ import java.util.*;
 
 class Solution {
     public int[] solution(long begin, long end) {
-        // 소수는 무조건 1, 나머지는 자기 자신을 제외한 가장 큰 값
         List<Integer> answer = new ArrayList<>();
-        
-        for (long i = begin; i <= end; i++) {
-            if (i == 1) {
+        for (long num = begin; num <= end; num++) {
+            if (num == 1) {
                 answer.add(0);
                 continue;
             }
             
-            answer.add(cal(i));
-        }
-        
-        return answer.stream().mapToInt(Integer::intValue).toArray();
-    }
-    
-    private int cal(long num) {
-        int max = 1;
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                max = i;
-                if (num / i <= 10000000) return (int) num / i;
+            int max = 1;
+            for (int i = 2; i * i <= num; i++) {
+                if (num % i == 0) {
+                    max = i;
+                    if (num / i <= 10_000_000) {
+                        max = (int) num / i;
+                        break;
+                    }
+                }
             }
+            answer.add(max);
         }
-        return max;
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
