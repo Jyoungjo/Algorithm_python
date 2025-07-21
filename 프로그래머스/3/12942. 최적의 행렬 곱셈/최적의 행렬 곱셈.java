53051c84a1901340e6b145c3e4e189ad8b0f77e6
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-07-21 12:05:20 +0900

    [level 3] Title: 최적의 행렬 곱셈, Time: 26.51 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/3/12942. 최적의 행렬 곱셈/최적의 행렬 곱셈.java
+++ 프로그래머스/3/12942. 최적의 행렬 곱셈/최적의 행렬 곱셈.java
@@ -1,32 +1,22 @@
 import java.util.*;
 
 class Solution {
-    int[][] dp;
-    int[][] matrix;
-    int len;
-    
     public int solution(int[][] matrix_sizes) {
-        len = matrix_sizes.length;
-        matrix = matrix_sizes;
-        dp = new int[len + 1][len + 1];
-        return cal(0, len);
-    }
-    
-    private int cal(int s, int e) {
-        if (e - s == 1) return 0;
+        int len = matrix_sizes.length, INF = 987654321;
+        int[][] dp = new int[len][len];
+        for (int i = 0; i < len; i++) Arrays.fill(dp[i], INF);
+        for (int i = 0; i < len; i++) dp[i][i] = 0;
         
-        int result = 987654321;
-        for (int m = s + 1; m < e; m++) {
-            int l = memo(s, m), r = memo(m, e);
-            int cur = matrix[s][0] * matrix[m][0] * matrix[e - 1][1];
-            result = Math.min(result, l + cur + r);
+        for (int l = 1; l < len; l++) {
+            for (int s = 0; s < len - l; s++) {
+                int e = s + l;
+                for (int m = s; m < e; m++) {
+                    dp[s][e] = Math.min(dp[s][e], dp[s][m] + dp[m + 1][e]
+                        + matrix_sizes[s][0] * matrix_sizes[m][1] * matrix_sizes[e][1]);
+                }
+            }
         }
         
-        return result;
-    }
-    
-    private int memo(int s, int e) {
-        if (dp[s][e] == 0) dp[s][e] = cal(s, e);
-        return dp[s][e];
+        return dp[0][len - 1];
     }
 }
