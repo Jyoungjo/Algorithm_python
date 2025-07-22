1fd6d38736091c37178728c3e0b2a1122c0ae989
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-07-22 11:02:38 +0900

    [level 3] Title: [1차] 추석 트래픽, Time: 65.98 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
+++ 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.java
@@ -2,42 +2,40 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] lines) {
-        int len = lines.length;
-        int[][] time_table = new int[len][2];
-        for (int i = 0; i < len; i++) {
-            String[] line = lines[i].split(" ");
-            int end = convertToMillisec(line[1]);
-            int T = convertFromExecuteTime(line[2]);
-            int start = end - T + 1;
-            time_table[i] = new int[]{start, end};
+        List<int[]> times = new ArrayList<>();
+        for (String line : lines) {
+            String[] splited = line.split(" ");
+            int S = convertToMilliSecFromTime(splited[1].split(":"));
+            int T = convertToMilliSecFromExecuteTime(splited[2].substring(0, splited[2].length() - 1).replace(".", ""));
+            
+            int start = S - T + 1;
+            times.add(new int[]{start, S});
         }
         
-        int answer = -1;
-        for (int i = 0; i < time_table.length; i++) {
-            int s = time_table[i][1], e = s + 999;
-            
+        int answer = 0;
+        for (int[] t : times) {
             int cnt = 0;
-            for (int[] time : time_table) {
-                if (!(s > time[1] || time[0] > e)) cnt++;
+            int s = t[1], e = s + 999;
+            for (int[] tt : times) {
+                if (tt[0] <= e && tt[1] >= s) cnt++;
             }
-            
             answer = Math.max(answer, cnt);
         }
+        
         return answer;
     }
     
-    private int convertToMillisec(String time) {
-        String[] t = time.split("[:.]");
-        int h = Integer.parseInt(t[0]) * 60 * 60 * 1000;
-        int m = Integer.parseInt(t[1]) * 60 * 1000;
-        int s = Integer.parseInt(t[2]) * 1000;
-        int ss = Integer.parseInt(t[3]);
+    private int convertToMilliSecFromTime(String[] time) {
+        int h = Integer.parseInt(time[0]) * 3600000;
+        int m = Integer.parseInt(time[1]) * 60000;
+        int s = Integer.parseInt(time[2].split("\\.")[0]) * 1000;
+        int ss = Integer.parseInt(time[2].split("\\.")[1]);
+        
         return h + m + s + ss;
     }
     
-    private int convertFromExecuteTime(String T) {
-        String t = T.substring(0, T.length() - 1).replace(".", "");
-        while (t.length() < 4) t += "0";
-        return Integer.parseInt(t);
+    private int convertToMilliSecFromExecuteTime(String s) {
+        while (s.length() < 4) s += "0";
+        return Integer.parseInt(s);
     }
 }
