bcecbaed9356f27371ff697456dce896e2bb661f
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-07-23 10:17:13 +0900

    [level 3] Title: 캠핑, Time: 1952.04 ms, Memory: 421 MB -BaekjoonHub

--- 프로그래머스/3/1833. 캠핑/캠핑.java
+++ 프로그래머스/3/1833. 캠핑/캠핑.java
@@ -1,34 +1,24 @@
 import java.util.*;
 
 class Solution {
+    List<Integer> xList, yList, uXList, uYList;
+    
     public int solution(int n, int[][] data) {
-        List<Integer> xList = new ArrayList<>();
-        List<Integer> yList = new ArrayList<>();
-        
-        for (int[] d : data) {
-            xList.add(d[0]);
-            yList.add(d[1]);
-        }
-        
-        List<Integer> uniqueXList = new ArrayList<>(new HashSet<>(xList));
-        List<Integer> uniqueYList = new ArrayList<>(new HashSet<>(yList));
-        
-        Collections.sort(uniqueXList);
-        Collections.sort(uniqueYList);
+        makeList(n, data);
         
         int[][] dp = new int[n][n];
         for (int i = 0; i < n; i++) {
-            data[i][0] = uniqueXList.indexOf(xList.get(i));
-            data[i][1] = uniqueYList.indexOf(yList.get(i));
+            data[i][0] = uXList.indexOf(xList.get(i));
+            data[i][1] = uYList.indexOf(yList.get(i));
             
             dp[data[i][1]][data[i][0]] = 1;
         }
         
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i >= 1) dp[i][j] += dp[i - 1][j];
-                if (j >= 1) dp[i][j] += dp[i][j - 1];
-                if (i >= 1 && j >= 1) dp[i][j] -= dp[i - 1][j - 1];
+                if (i - 1 >= 0) dp[i][j] += dp[i - 1][j];
+                if (j - 1 >= 0) dp[i][j] += dp[i][j - 1];
+                if (i - 1 >= 0 && j - 1 >= 0) dp[i][j] -= dp[i - 1][j - 1];
             }
         }
         
@@ -42,11 +32,27 @@ class Solution {
                 
                 if (r1 == r2 || c1 == c2) continue;
                 
-                int blank = dp[r2 - 1][c2 - 1] - dp[r2 - 1][c1] - dp[r1][c2 - 1] + dp[r1][c1];
-                if (blank == 0) answer++;
+                int cnt = dp[r2 - 1][c2 - 1] - dp[r2 - 1][c1] - dp[r1][c2 - 1] + dp[r1][c1];
+                if (cnt == 0) answer++;
             }
         }
         
         return answer;
+    }
+    
+    private void makeList(int n, int[][] data) {
+        xList = new ArrayList<>();
+        yList = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+            xList.add(data[i][0]);
+            yList.add(data[i][1]);
+        }
+        
+        uXList = new ArrayList<>(new HashSet<>(xList));
+        uYList = new ArrayList<>(new HashSet<>(yList));
+        
+        Collections.sort(uXList);
+        Collections.sort(uYList);
     }
 }
