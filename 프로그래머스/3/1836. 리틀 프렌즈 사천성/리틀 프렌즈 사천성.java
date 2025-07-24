6eabd32a1a8be1ddf242a4a8626e1111a11a7ec3
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-07-24 11:01:04 +0900

    [level 3] Title: 리틀 프렌즈 사천성, Time: 24.45 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/3/1836. 리틀 프렌즈 사천성/리틀 프렌즈 사천성.java
+++ 프로그래머스/3/1836. 리틀 프렌즈 사천성/리틀 프렌즈 사천성.java
@@ -1,92 +1,75 @@
 import java.util.*;
 
 class Solution {
-    final String IMPOSSIBLE = "IMPOSSIBLE";
-    final char BLANK = '.';
-    Map<Character, List<int[]>> tiles;
-    char[][] BOARD;
+    char[][] n_board;
     int M, N;
+    Map<Character, List<int[]>> map = new HashMap<>();
+    StringBuilder sb = new StringBuilder();
     
     public String solution(int m, int n, String[] board) {
-        init(m, n, board);
-        return play();
-    }
-    
-    private void init(int m, int n, String[] board) {
-        tiles = new HashMap<>();
         M = m; N = n;
-        BOARD = new char[m][n];
+        n_board = new char[m][n];
+        for (int i = 0; i < m; i++) n_board[i] = board[i].toCharArray();
+        putToMap();
         
-        for (int i = 0; i < m; i++) {
-            BOARD[i] = board[i].toCharArray();
+        List<Character> keys = new LinkedList<>(map.keySet());
+        Collections.sort(keys);
+        int idx = 0;
+        
+        while (!keys.isEmpty()) {
+            char now = keys.get(idx);
+            if (canRemove(now)) {
+                removeBlock(now);
+                keys.remove(idx);
+                idx = 0;
+            } else {
+                idx++;
+                if (idx == keys.size()) return "IMPOSSIBLE";
+            }
         }
         
+        return sb.toString();
+    }
+    
+    private void putToMap() {        
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
-                char now = board[i].charAt(j);
-                if ('A' <= now && now <= 'Z') {
-                    tiles.putIfAbsent(now, new ArrayList<>());
-                    tiles.get(now).add(new int[]{i, j});
+                if ('A' <= n_board[i][j] && n_board[i][j] <= 'Z') {
+                    map.putIfAbsent(n_board[i][j], new ArrayList<>());
+                    map.get(n_board[i][j]).add(new int[]{i, j});
                 }
             }
         }
     }
     
-    private String play() {
-        String result = "";
-        List<Character> alpha = new LinkedList<>(tiles.keySet());
-        Collections.sort(alpha);
-        
-        int idx = 0;
-        while (!alpha.isEmpty()) {
-            char now = alpha.get(idx);
-            if (canRemove(now)) {
-                alpha.remove(idx);
-                remove(now);
-                idx = 0;
-                result += now;
-            } else {
-                idx++;
-                if (idx == alpha.size()) return IMPOSSIBLE;
-            }
-        }
-        return result;
-    }
-    
-    private boolean canRemove(char alpha) {
-        List<int[]> coords = tiles.get(alpha);
-        int y1 = coords.get(0)[0], x1 = coords.get(0)[1], y2 = coords.get(1)[0], x2 = coords.get(1)[1];
+    private boolean canRemove(char now) {
+        List<int[]> coord = map.get(now);
+        int[] first = coord.get(0), second = coord.get(1);
+        int y1 = first[0], x1 = first[1], y2 = second[0], x2 = second[1];
         
         if (x1 < x2) {
-            if (isEmptyRow(alpha, y1, y2, x1) && isEmptyColumn(alpha, x1, x2, y2)) return true;
-            if (isEmptyRow(alpha, y1, y2, x2) && isEmptyColumn(alpha, x1, x2, y1)) return true;
+            if (hasOneValInRow(now, x1, x2, y1) && hasOneValInCol(now, y1, y2, x2)) return true;
+            if (hasOneValInRow(now, x1, x2, y2) && hasOneValInCol(now, y1, y2, x1)) return true;
         } else {
-            if (isEmptyRow(alpha, y1, y2, x2) && isEmptyColumn(alpha, x2, x1, y1)) return true;
-            if (isEmptyRow(alpha, y1, y2, x1) && isEmptyColumn(alpha, x2, x1, y2)) return true;
+            if (hasOneValInRow(now, x2, x1, y2) && hasOneValInCol(now, y1, y2, x1)) return true;
+            if (hasOneValInRow(now, x2, x1, y1) && hasOneValInCol(now, y1, y2, x2)) return true;
         }
         
         return false;
     }
     
-    private boolean isEmptyRow(char target, int r1, int r2, int c) {
-        for (int r = r1; r <= r2; r++) {
-            if (BOARD[r][c] != BLANK && BOARD[r][c] != target) return false;
-        }
-        
+    private boolean hasOneValInRow(char now, int c1, int c2, int r) {
+        for (int c = c1; c <= c2; c++) if (n_board[r][c] != '.' && n_board[r][c] != now) return false;
         return true;
     }
     
-    private boolean isEmptyColumn(char target, int c1, int c2, int r) {
-        for (int c = c1; c <= c2; c++) {
-            if (BOARD[r][c] != BLANK && BOARD[r][c] != target) return false;
-        }
-        
+    private boolean hasOneValInCol(char now, int r1, int r2, int c) {
+        for (int r = r1; r <= r2; r++) if (n_board[r][c] != '.' && n_board[r][c] != now) return false;
         return true;
     }
     
-    private void remove(char target) {
-        List<int[]> coords = tiles.get(target);
-        BOARD[coords.get(0)[0]][coords.get(0)[1]] = BLANK;
-        BOARD[coords.get(1)[0]][coords.get(1)[1]] = BLANK;
+    private void removeBlock(char now) {
+        for (int[] coord : map.get(now)) n_board[coord[0]][coord[1]] = '.';
+        sb.append(now);
     }
 }
