e73b08c7ba735ef0474455ab0b28a4ddae1c9f41
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-07-28 10:44:08 +0900

    [level 3] Title: 몸짱 트레이너 라이언의 고민, Time: 62.07 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/3/1838. 몸짱 트레이너 라이언의 고민/몸짱 트레이너 라이언의 고민.java
+++ 프로그래머스/3/1838. 몸짱 트레이너 라이언의 고민/몸짱 트레이너 라이언의 고민.java
@@ -1,65 +1,69 @@
 import java.util.*;
 
 class Solution {
-    int N;
+    int N, M;
     
     public int solution(int n, int m, int[][] timetable) {
-        N = n;
-        int max = calMax(timetable);
-        if (max <= 1) return 0;
+        N = n; M = m;
         
-        return solve(max);
-    }
-    
-    private int calMax(int[][] timetable) {
-        int[] people = new int[1321];
+        int cnt = cntMember(timetable);
+        if (cnt <= 1) return 0;
         
-        for (int[] time : timetable) {
-            for (int i = time[0]; i <= time[1]; i++) people[i]++;
-        }
-        
-        int max = 0;
-        for (int i = 600; i < 1321; i++) max = Math.max(max, people[i]);
-        
-        return max;
-    }
-    
-    private int solve(int people) {
-        int l = 1, r = N * 2, res = 0;
-        
+        int l = 0, r = (n - 1) * 2, answer = 0;
         while (l <= r) {
             int mid = (l + r) / 2;
             
-            if (canAssign(people, mid)) {
-                res = mid;
-                l = mid + 1;
+            if (canAssign(cnt, mid)) {
+                answer = mid;
+                l = mid + 1; // 최대한 멀리 떨어져야 하므로
             } else r = mid - 1;
         }
         
-        return res;
+        return answer;
     }
     
-    private boolean canAssign(int people, int dist) {
+    private int cntMember(int[][] timetable) {
+        List<int[]> list = new ArrayList<>();
+        for (int[] t : timetable) {
+            list.add(new int[]{t[0], 1});
+            list.add(new int[]{t[1], -1});
+        }
+        
+        list.sort((a, b) -> {
+            if (a[0] == b[0]) return b[1] - a[1];
+            return a[0] - b[0];
+        });
+        
+        int ans = 0, max = 0;
+        for (int[] t : list) {
+            max += t[1];
+            ans = Math.max(ans, max);
+        }
+        
+        return ans;
+    }
+    
+    private boolean canAssign(int member, int dist) {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                List<int[]> assigned = new ArrayList<>();
-                assigned.add(new int[]{i, j});
+                List<int[]> placed = new ArrayList<>();
+                placed.add(new int[]{i, j});
                 
-                for (int row = i; row < N; row++) {
-                    for (int col = 0; col < N; col++) {
-                        boolean flag = true;
-                        if (row == i && col <= j) continue;
+                for (int r = i; r < N; r++) {
+                    for (int c = 0; c < N; c++) {
+                        if (r == i && c <= j) continue;
                         
-                        for (int[] a : assigned) {
-                            if (Math.abs(a[0] - row) + Math.abs(a[1] - col) < dist) {
+                        boolean flag = true;
+                        for (int[] p : placed) {
+                            if (Math.abs(p[0] - r) + Math.abs(p[1] - c) < dist) {
                                 flag = false;
                                 break;
                             }
                         }
                         
                         if (flag) {
-                            assigned.add(new int[]{row, col});
-                            if (assigned.size() == people) return true;
+                            placed.add(new int[]{r, c});
+                            if (placed.size() == member) return true;
                         }
                     }
                 }
