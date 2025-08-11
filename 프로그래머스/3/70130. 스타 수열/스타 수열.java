367b919cc1d21f300c3509bfaade485a61195686
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-08-11 14:31:42 +0900

    [level 3] Title: 스타 수열, Time: 37.51 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/70130. 스타 수열/스타 수열.java
+++ 프로그래머스/3/70130. 스타 수열/스타 수열.java
@@ -2,28 +2,23 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] a) {
-        /*
-            1. 길이 2 이상의 짝수
-            2. 두 개씩 짝 지었을 때, 교집합 원소 개수 1 이상
-            3. 두 개 비교했을 때, 두 원소가 달라야 함.
-        */
-        
-        int[] cnt = new int[a.length + 1];
-        for (int i = 0; i < a.length; i++) cnt[a[i]]++;
+        int len = a.length;
+        int[] cnts = new int[len];
+        for (int i = 0; i < len; i++) cnts[a[i]]++;
         
         int answer = -1;
-        for (int i = 0; i < cnt.length; i++) {
-            if (answer >= cnt[i]) continue;
+        for (int i = 0; i < len; i++) {
+            if (cnts[i] <= answer) continue;
             
-            int res = 0;
-            for (int j = 0; j < a.length - 1; j++) {
+            int cnt = 0;
+            for (int j = 0; j < len - 1; j++) {
                 if (a[j] != a[j + 1] && (a[j] == i || a[j + 1] == i)) {
-                    res++;
                     j++;
+                    cnt++;
                 }
             }
             
-            answer = Math.max(answer, res);
+            answer = Math.max(answer, cnt);
         }
         
         return answer * 2;
