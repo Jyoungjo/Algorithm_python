fbaf15a7bb8e305ce3b100434999943f9b85cb70
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-08-17 16:22:48 +0900

    [level 3] Title: 외벽 점검, Time: 88.90 ms, Memory: 95.7 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
@@ -1,42 +1,44 @@
 import java.util.*;
 
 class Solution {
-    int len;
-    int[] w;
+    int[] WEAK;
     int answer = 987654321;
     
     public int solution(int n, int[] weak, int[] dist) {
-        len = weak.length;
-        w = new int[len * 2];
+        int len = weak.length;
+        WEAK = new int[len * 2];
         for (int i = 0; i < len; i++) {
-            w[i] = weak[i];
-            w[i + len] = weak[i] + n;
+            WEAK[i] = weak[i];
+            WEAK[i + len] = weak[i] + n;
         }
-        permutations(dist, 0, new ArrayList<>());
+        
+        permutations(new ArrayList<>(), dist, 0);
+        
         return answer == 987654321 ? -1 : answer;
     }
     
-    private void permutations(int[] dist, int mask, List<Integer> list) {
-        if (list.size() >= answer) return;
+    private void permutations(List<Integer> list, int[] dist, int masked) {
+        if (answer <= list.size()) return;
         
         for (int i = 0; i < dist.length; i++) {
-            if ((mask & (1 << i)) != 0) continue;
+            if ((masked & (1 << i)) != 0) continue;
             List<Integer> tmp = new ArrayList<>(list);
             tmp.add(dist[i]);
-            exe(tmp);
-            permutations(dist, mask | (1 << i), tmp);
+            check(tmp);
+            permutations(tmp, dist, masked | (1 << i));
         }
     }
     
-    private void exe(List<Integer> list) {
-        for (int i = 0; i < len; i++) {
+    private void check(List<Integer> list) {
+        for (int i = 0; i < WEAK.length / 2; i++) {
             int idx = i;
-            for (int n : list) {
-                int pos = w[idx] + n;
-                while (idx < w.length && pos >= w[idx]) idx++;
+            
+            for (int dist : list) {
+                int pos = WEAK[idx] + dist;
+                while (idx < WEAK.length && pos >= WEAK[idx]) idx++;
             }
             
-            if (idx - i >= len) {
+            if (idx - i >= WEAK.length / 2) {
                 answer = Math.min(answer, list.size());
                 return;
             }
