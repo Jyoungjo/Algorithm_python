57acba90186a66f656886e1c94cfc031effbfb42
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-08-18 15:22:56 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 196.74 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.java
@@ -1,30 +1,30 @@
 import java.util.*;
 
 class Solution {
+    List<int[]> keys = new ArrayList<>();
+    Set<String> locks = new HashSet<>();
     int M, N;
     
     public boolean solution(int[][] key, int[][] lock) {
         M = key.length; N = lock.length;
-        Set<String> lockSet = new HashSet<>();
-        List<int[]> keyList = new ArrayList<>();
         
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < M; j++) {
-                if (key[i][j] == 1) keyList.add(new int[]{i, j});
+                if (key[i][j] == 1) keys.add(new int[]{i, j});
             }
         }
         
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (lock[i][j] == 0) lockSet.add(i + "," + j);
+                if (lock[i][j] == 0) locks.add(i + "," + j);
             }
         }
         
-        for (int rot = 0; rot < 4; rot++) {
-            List<int[]> rotated = rotate(keyList, rot);
+        for (int d = 0; d < 4; d++) {
+            rotate();
             for (int dy = -M + 1; dy < N; dy++) {
                 for (int dx = -M + 1; dx < N; dx++) {
-                    if (isMatch(rotated, lock, lockSet, dy, dx)) return true;
+                    if (isMatch(lock, dy, dx)) return true;
                 }
             }
         }
@@ -32,36 +32,36 @@ class Solution {
         return false;
     }
     
-    private List<int[]> rotate(List<int[]> key, int rot) {
-        List<int[]> result = new ArrayList<>();
-        for (int[] k : key) {
+    private void rotate() {
+        for (int[] k : keys) {
             int y = k[0], x = k[1];
-            for (int i = 0; i < rot; i++) {
-                int tmp = y; y = x; x = M - 1 - tmp;
-            }
-            result.add(new int[]{y, x});
+            int tmp = y;
+            y = x;
+            x = M - tmp - 1;
+            k[0] = y;
+            k[1] = x;
         }
-        return result;
     }
     
-    private boolean isMatch(List<int[]> key, int[][] lock, Set<String> lockSet, int dy, int dx) {
-        Set<String> keySet = new HashSet<>();
-        for (int[] k : key) keySet.add((k[0] + dy) + "," + (k[1] + dx));
-        
-        for (String l : lockSet) {
-            if (!keySet.contains(l)) return false;
+    private boolean isMatch(int[][] lock, int dy, int dx) {
+        Set<String> keysSet = new HashSet<>();
+        for (int[] k : keys) {
+            int yy = k[0] + dy, xx = k[1] + dx;
+            keysSet.add(yy + "," + xx);
         }
-        
-        for (String k : keySet) {
-            String[] tmp = k.split(",");
-            int y = Integer.parseInt(tmp[0]), x = Integer.parseInt(tmp[1]);
-            if (isWithinRange(y, x) && lock[y][x] == 1) return false;
+                
+        for (String l : locks) {
+            if (!keysSet.contains(l)) return false;
+        }
+                
+        for (String k : keysSet) {
+            String[] kk = k.split(",");
+            int y = Integer.parseInt(kk[0]), x = Integer.parseInt(kk[1]);
+            if (0 <= y && y < N && 0 <= x && x < N) {
+                if (lock[y][x] == 1) return false;
+            }
         }
         
         return true;
-    }
-    
-    private boolean isWithinRange(int y, int x) {
-        return 0 <= y && y < N && 0 <= x && x < N;
     }
 }
