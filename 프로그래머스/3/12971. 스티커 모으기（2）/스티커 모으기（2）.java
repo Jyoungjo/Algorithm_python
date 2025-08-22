c4009120bc2bd6d6cce8f14a1c89c550311b2e1f
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-08-22 16:22:02 +0900

    [level 3] Title: 스티커 모으기(2), Time: 10.69 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -1,21 +1,17 @@
 class Solution {
     public int solution(int sticker[]) {
         int answer = 0;
-        int len = sticker.length;
-        if (len == 1) return sticker[0];
-        
-        for (int i = 0; i < 2; i++) {
-            int[] dp = new int[len - 1];
+        if (sticker.length == 1) return sticker[0];
+        for (int i = 0; i <= 1; i++) {
+            int[] dp = new int[sticker.length - 1];
             dp[0] = sticker[i];
-            
-            for (int j = 1; j < len - 1; j++) {
-                if (j == 1) dp[j] = Math.max(dp[j - 1], sticker[i + j]);
-                else dp[j] = Math.max(dp[j - 1], dp[j - 2] + sticker[i + j]);
+            for (int j = 1; j < sticker.length - 1; j++) {
+                if (j == 1) dp[j] = Math.max(dp[j - 1], sticker[j + i]);
+                else dp[j] = Math.max(dp[j - 2] + sticker[i + j], dp[j - 1]);
             }
             
-            answer = Math.max(answer, dp[len - 2]);
+            answer = Math.max(answer, dp[sticker.length - 2]);
         }
-        
         return answer;
     }
 }
