5ae4cf6db5fbbf70941a4b7e6b7c878ab10c774b
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-08-31 00:38:15 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 216 ms, Memory: 15804 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
@@ -3,87 +3,80 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
-    static int N, K;
-    static int[] conveyor;
-    static boolean[] robot;
+class Conveyor {
+    private int durability;
+    private boolean onRobot;
 
-    public static int solution() {
+    Conveyor(int durability) {
+        this.durability = durability;
+        this.onRobot = false;
+    }
+
+    int getDurability() { return this.durability; }
+    boolean getOnRobot() { return this.onRobot; }
+
+    void putRobot() { this.onRobot = true; }
+    void unloadRobot() { this.onRobot = false; }
+    void decreaseDurability() { this.durability--; }
+}
+
+class Main {
+    int N, K;
+    Conveyor[] conveyors;
+    int cnt = 0;
+
+    private void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        conveyors = Arrays.stream(br.readLine().split(" "))
+                .map(d -> new Conveyor(Integer.parseInt(d)))
+                .toArray(Conveyor[]::new);
+
         int answer = 0;
-        while (isOk()) {
-            // 1. 벨트 회전 (위에 있는 로봇도 같이 회전)
-            rotateConveyor();
-            // 2. 벨트 위 로봇들 이동 (로봇 이동 시 앞에 로봇 없고 내구도 1 이상)
-            moveRobot();
-            // 3. 올리는 위치에 로봇 올림 -> 올렸을 때 내구도 0인 벨트가 K개 라면 종료
-            putRobot(0);
+        while (cnt < K) {
+            answer++;
 
-            answer++; // 단계 끝나면 카운트 증가
+            // 회전
+            rotate();
+            // 로봇 이동
+            move();
+            // 로봇 올림
+            put(0);
         }
 
-        return answer;
+        System.out.println(answer);
     }
 
-    private static void rotateConveyor() {
-        int tmp = conveyor[conveyor.length - 1];
-        for (int i = conveyor.length - 1; i > 0; i--) {
-            conveyor[i] = conveyor[i - 1];
-        }
-        conveyor[0] = tmp;
+    private void rotate() {
+        Conveyor tmp = conveyors[conveyors.length - 1];
+        for (int i = conveyors.length - 1; i >= 1; i--) conveyors[i] = conveyors[i - 1];
+        conveyors[0] = tmp;
 
-        for (int i = robot.length - 1; i > 0; i--) {
-            robot[i] = robot[i - 1];
-        }
-
-        // 맨 처음과 끝은 이동 후에 로봇이 존재할 수 없음
-        robot[0] = false;
-        robot[N - 1] = false;
+        conveyors[0].unloadRobot();
+        conveyors[N - 1].unloadRobot();
     }
 
-    private static void moveRobot() {
-        for (int i = N - 1; i > 0; i--) {
-            if (!robot[i] && robot[i - 1] && conveyor[i] >= 1) {
-                robot[i - 1] = false;
-                putRobot(i);
+    private void move() {
+        for (int i = N - 1; i >= 1; i--) {
+            if (!conveyors[i].getOnRobot() && conveyors[i - 1].getOnRobot() && conveyors[i].getDurability() >= 1) {
+                conveyors[i - 1].unloadRobot();
+                put(i);
             }
         }
-        robot[N - 1] = false;
+        conveyors[N - 1].unloadRobot();
     }
 
-    private static void putRobot(int i) {
-        if (conveyor[i] > 0) {
-            robot[i] = true;
-            conveyor[i]--;
+    private void put(int idx) {
+        if (conveyors[idx].getDurability() >= 1) {
+            conveyors[idx].putRobot();
+            conveyors[idx].decreaseDurability();
+            if (conveyors[idx].getDurability() == 0) cnt++;
         }
-    }
-
-    private static boolean isOk() {
-        int cnt = 0;
-        for (int c : conveyor) {
-            if (c == 0) {
-                cnt++;
-            }
-
-            if (cnt >= K) return false;
-        }
-
-        return true;
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-
-        conveyor = new int[2 * N];
-        robot = new boolean[N];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 2 * N; i++) {
-            conveyor[i] = Integer.parseInt(st.nextToken());
-        }
-
-        System.out.println(solution());
+        new Main().solution();
     }
 }
