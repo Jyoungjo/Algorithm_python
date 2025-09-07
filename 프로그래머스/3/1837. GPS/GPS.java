e0ca58c1585664d9e7b562496999d1fd2ba601cd
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-09-07 16:52:15 +0900

    [level 3] Title: GPS, Time: 75.64 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/3/1837. GPS/GPS.java
+++ 프로그래머스/3/1837. GPS/GPS.java
@@ -1,31 +1,34 @@
 import java.util.*;
 
 class Solution {
-    List<Integer>[] graph;
+    final int MAX = 987654321;
+    int N, M;
+    List<Integer>[] edges;
     
     public int solution(int n, int m, int[][] edge_list, int k, int[] gps_log) {
-        graph = new List[n + 1];
-        for (int i = 1; i <= n; i++) graph[i] = new ArrayList<>();
+        N = n; M = m;
+        edges = new List[n + 1];
+        for (int i = 1; i <= N; i++) edges[i] = new ArrayList<>();
         for (int[] edge : edge_list) {
-            graph[edge[0]].add(edge[1]);
-            graph[edge[1]].add(edge[0]);
+            edges[edge[0]].add(edge[1]);
+            edges[edge[1]].add(edge[0]);
         }
         
         int[][] dp = new int[k][n + 1];
-        for (int i = 0; i < k; i++) Arrays.fill(dp[i], 987654321);
+        for (int i = 0; i < k; i++) Arrays.fill(dp[i], MAX);
         dp[0][gps_log[0]] = 0;
         
         for (int i = 1; i < k; i++) {
             for (int j = 1; j <= n; j++) {
-                int prev = dp[i - 1][j];
+                int prev = dp[i - 1][j], goal = gps_log[i];
                 
-                for (int next : graph[j]) {
-                    if (next == gps_log[i]) dp[i][next] = Math.min(dp[i][next], prev);
+                for (int next : edges[j]) {
+                    if (next == goal) dp[i][next] = Math.min(dp[i][next], prev);
                     else dp[i][next] = Math.min(dp[i][next], prev + 1);
                 }
             }
         }
         
-        return dp[k - 1][gps_log[gps_log.length - 1]] == 987654321 ? -1 : dp[k - 1][gps_log[gps_log.length - 1]];
+        return dp[k - 1][gps_log[gps_log.length - 1]] == MAX ? -1 : dp[k - 1][gps_log[gps_log.length - 1]];
     }
 }
