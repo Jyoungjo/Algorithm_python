fc084e8bba4fe8de34bb15303c03f44b1c113969
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-09-07 16:07:37 +0900

    [level 3] Title: 캠핑, Time: 1793.06 ms, Memory: 401 MB -BaekjoonHub

--- 프로그래머스/3/1833. 캠핑/캠핑.java
+++ 프로그래머스/3/1833. 캠핑/캠핑.java
@@ -1,36 +1,26 @@
 import java.util.*;
 
+class CoordinateCompressor {
+    
+    CoordinateCompressor() {}
+    
+    public void compress(List<Integer> list, int n, int[][] data, int idx) {
+        List<Integer> uniqueList = new ArrayList<>(new HashSet<>(list));
+        Collections.sort(uniqueList);
+        for (int i = 0; i < n; i++) data[i][idx] = uniqueList.indexOf(list.get(i));
+    }
+}
+
 class Solution {
     public int solution(int n, int[][] data) {
-        List<Integer> xList = new ArrayList<>();
-        List<Integer> yList = new ArrayList<>();
+        List<Integer> xList = getList(data, 0);
+        List<Integer> yList = getList(data, 1);
         
-        for (int[] d : data) {
-            xList.add(d[0]);
-            yList.add(d[1]);
-        }
+        CoordinateCompressor compressor = new CoordinateCompressor();
+        compressor.compress(xList, n, data, 0);
+        compressor.compress(yList, n, data, 1);
         
-        List<Integer> compressedX = new ArrayList<>(new HashSet<>(xList));
-        List<Integer> compressedY = new ArrayList<>(new HashSet<>(yList));
-        
-        Collections.sort(compressedX);
-        Collections.sort(compressedY);
-        
-        int[][] dp = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            data[i][0] = compressedX.indexOf(xList.get(i));
-            data[i][1] = compressedY.indexOf(yList.get(i));
-            
-            dp[data[i][1]][data[i][0]] = 1;
-        }
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i > 0) dp[i][j] += dp[i - 1][j];
-                if (j > 0) dp[i][j] += dp[i][j - 1];
-                if (i > 0 && j > 0) dp[i][j] -= dp[i - 1][j - 1];
-            }
-        }
+        int[][] dp = getDp(n, data);
         
         Arrays.sort(data, (o1, o2) -> {
             if (o1[0] == o2[0]) return o1[1] - o2[1];
@@ -48,11 +38,31 @@ class Solution {
                 if (r1 == r2 || c1 == c2) continue;
                 
                 int cnt = dp[r2 - 1][c2 - 1] - dp[r2 - 1][c1] - dp[r1][c2 - 1] + dp[r1][c1];
-                
                 if (cnt == 0) answer++;
             }
         }
         
         return answer;
+    }
+    
+    private List<Integer> getList(int[][] data, int idx) {
+        List<Integer> result = new ArrayList<>();
+        for (int[] d : data) result.add(d[idx]);
+        return result;
+    }
+    
+    private int[][] getDp(int n, int[][] data) {
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) dp[data[i][1]][data[i][0]] = 1;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i > 0) dp[i][j] += dp[i - 1][j];
+                if (j > 0) dp[i][j] += dp[i][j - 1];
+                if (i > 0 && j > 0) dp[i][j] -= dp[i - 1][j - 1];
+            }
+        }
+        
+        return dp;
     }
 }
