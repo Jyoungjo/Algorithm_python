9491274ef6cc6e83c447779dea9ea257c3360975
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-09-14 15:52:50 +0900

    [Gold III] Title: ACM Craft, Time: 724 ms, Memory: 237956 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -1,53 +1,49 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 class Main {
-    static int[] dp;
+    int T, N, K;
+    int[] build_time, inDegree, dp;
+    List<Integer>[] build_ordered;
 
-    public static void main(String[] args) throws IOException {
+    private void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 개수
-        for (int t = 0; t < T; t++) {
-            // N, K
+        T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken()), K = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
 
-            int[] times = new int[N + 1];
-            List<Integer>[] buildings = new List[N + 1];
-            int[] inDegree = new int[N + 1];
-
-            // 건물 짓는데 걸리는 시간
             st = new StringTokenizer(br.readLine());
-            for (int i = 1; i <= N; i++) {
-                times[i] = Integer.parseInt(st.nextToken());
-                buildings[i] = new ArrayList<>();
+            build_time = new int[N + 1];
+            inDegree = new int[N + 1];
+            dp = new int[N + 1];
+            build_ordered = new List[N + 1];
+            for (int j = 1; j <= N; j++) {
+                build_time[j] = Integer.parseInt(st.nextToken());
+                build_ordered[j] = new ArrayList<>();
             }
 
-            // 건설 순서
-            for (int i = 0; i < K; i++) {
+            for (int j = 0; j < K; j++) {
                 st = new StringTokenizer(br.readLine());
-                int s = Integer.parseInt(st.nextToken()), e = Integer.parseInt(st.nextToken());
-                buildings[s].add(e);
+                int s = Integer.parseInt(st.nextToken());
+                int e = Integer.parseInt(st.nextToken());
+                build_ordered[s].add(e);
                 inDegree[e]++;
             }
 
-            // 찾는 건물
             int W = Integer.parseInt(br.readLine());
-
-            dp = new int[N + 1];
-            play(N, times, buildings, inDegree);
-
+            play();
             System.out.println(dp[W]);
         }
     }
 
-    private static void play(int N, int[] times, List<Integer>[] buildings, int[] inDegree) {
+    private void play() {
         Queue<Integer> q = new LinkedList<>();
         for (int i = 1; i <= N; i++) {
             if (inDegree[i] == 0) {
-                dp[i] = times[i];
+                dp[i] = build_time[i];
                 q.add(i);
             }
         }
@@ -55,11 +51,15 @@ class Main {
         while (!q.isEmpty()) {
             int now = q.poll();
 
-            for (int next : buildings[now]) {
-                dp[next] = Math.max(dp[now] + times[next], dp[next]);
+            for (int next : build_ordered[now]) {
+                dp[next] = Math.max(dp[next], dp[now] + build_time[next]);
                 inDegree[next]--;
                 if (inDegree[next] == 0) q.add(next);
             }
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
     }
 }
