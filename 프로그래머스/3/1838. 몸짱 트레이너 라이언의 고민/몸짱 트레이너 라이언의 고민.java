85aab6729fbb758ef0f064349c151c77c9d5e683
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-09-23 14:32:29 +0900

    [level 3] Title: 몸짱 트레이너 라이언의 고민, Time: 89.06 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/3/1838. 몸짱 트레이너 라이언의 고민/몸짱 트레이너 라이언의 고민.java
+++ 프로그래머스/3/1838. 몸짱 트레이너 라이언의 고민/몸짱 트레이너 라이언의 고민.java
@@ -1,74 +1,69 @@
 import java.util.*;
 
 class Solution {
-    int N, M;
-    
     public int solution(int n, int m, int[][] timetable) {
-        if (m == 1) return 0;
+        if (timetable.length == 1) return 0;
         
-        N = n; M = m;
-        int people = findOverlappedPeople(timetable);
-        return calDist(people);
+        int people = findMaxPeople(timetable);
+        return assign(n, people);
     }
     
-    private int findOverlappedPeople(int[][] timetable) {
-        Arrays.sort(timetable, (o1, o2) -> {
-            if (o1[1] == o2[1]) return o1[0] - o2[0];
-            return o1[1] - o2[1];
-        });
-        
-        int people = Integer.MIN_VALUE;
-        for (int i = 0; i < M; i++) {
-            int tmp = 0;
-            int[] now = timetable[i];
-            
-            for (int j = i + 1; j < M; j++) {
-                int[] next = timetable[j];
-                
-                if (next[1] < now[0] || now[1] < next[0]) continue;
-                tmp++;
-            }
-            people = Math.max(tmp, people);
+    private int findMaxPeople(int[][] timetable) {
+        List<int[]> events = new ArrayList<>();
+        for (int[] t : timetable) {
+            events.add(new int[]{t[0], 1});
+            events.add(new int[]{t[1], -1});
         }
         
-        return people == 0 ? people : people + 1;
+        events.sort((o1, o2) -> {
+            if (o1[0] == o2[0]) return o2[1] - o1[1];
+            return o1[0] - o2[0];
+        });
+        
+        int max = -1, cnt = 0;
+        for (int[] e : events) {
+            cnt += e[1];
+            max = Math.max(max, cnt);
+        }
+        
+        return max;
     }
     
-    private int calDist(int people) {
-        int l = 0, r = (N - 1) * 2, result = 0;
-        
+    private int assign(int n, int people) {
+        int l = 1, r = n * 2, res = 0;
         while (l <= r) {
             int mid = l + ((r - l) >> 1);
-            if (assign(mid, people)) {
+            if (isAssign(n, mid, people)) {
                 l = mid + 1;
-                result = mid;
+                res = mid;
             } else r = mid - 1;
         }
         
-        return result;
+        return res;
     }
     
-    private boolean assign(int dist, int people) {
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < N; c++) {
-                List<int[]> lockers = new ArrayList<>();
-                lockers.add(new int[]{r, c});
+    private boolean isAssign(int n, int dist, int people) {
+        int[][] locker = new int[n][n];
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+                List<int[]> placed = new ArrayList<>();
+                placed.add(new int[]{r, c});
                 
-                for (int i = r; i < N; i++) {
-                    for (int j = 0; j < N; j++) {
-                        if (i == r && j <= c) continue;
+                for (int i = r; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        if (r == i && c >= j) continue;
                         
-                        boolean isAssigned = true;
-                        for (int[] l : lockers) {
-                            if (Math.abs(l[0] - i) + Math.abs(l[1] - j) < dist) {
-                                isAssigned = false;
+                        boolean flag = true;
+                        for (int[] p : placed) {
+                            if (Math.abs(p[0] - i) + Math.abs(p[1] - j) < dist) {
+                                flag = false;
                                 break;
                             }
                         }
                         
-                        if (isAssigned) {
-                            lockers.add(new int[]{i, j});
-                            if (lockers.size() == people) return true;
+                        if (flag) {
+                            placed.add(new int[]{i, j});
+                            if (placed.size() == people) return true;
                         }
                     }
                 }
