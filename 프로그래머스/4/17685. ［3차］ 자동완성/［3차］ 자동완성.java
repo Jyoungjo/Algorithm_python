86265f38d72347e14386b2756c99d42703ccc336
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-10-08 20:18:05 +0900

    [level 4] Title: [3차] 자동완성, Time: 241.62 ms, Memory: 306 MB -BaekjoonHub

---
+++ 프로그래머스/4/17685. ［3차］ 자동완성/［3차］ 자동완성.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Node {
+    Map<Character, Node> child = new HashMap<>();
+    int cnt;
+}
+
+class Trie {
+    Node root;
+    
+    Trie() { this.root = new Node(); }
+    
+    void insert(String word) {
+        Node node = this.root;
+        
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            
+            node.child.putIfAbsent(c, new Node());
+            node = node.child.get(c);
+            node.cnt++;
+        }
+    }
+    
+    int search(String word) {
+        int cnt = 0;
+        Node node = this.root;
+        
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            
+            node = node.child.get(c);
+            cnt++;
+            
+            if (node.cnt == 1) return cnt;
+        }
+        
+        return cnt;
+    }
+}
+
+class Solution {
+    public int solution(String[] words) {
+        int answer = 0;
+        Trie trie = new Trie();
+        for (String word : words) trie.insert(word);
+        for (String word : words) answer += trie.search(word);
+        
+        return answer;
+    }
+}
