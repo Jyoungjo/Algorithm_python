629963eec6176d1de7e4a6a66911b16f7ab5f75d
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-10-27 18:48:15 +0900

    [level 4] Title: 쿠키 구입, Time: 18.55 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/4/49995. 쿠키 구입/쿠키 구입.java
+++ 프로그래머스/4/49995. 쿠키 구입/쿠키 구입.java
@@ -1,17 +1,17 @@
 class Solution {
     public int solution(int[] cookie) {
         int answer = 0;
-        int size = cookie.length;
-        
-        for (int mid = 0; mid < size - 1; mid++) {
-            int lsum = cookie[mid], rsum = cookie[mid + 1];
+        int len = cookie.length;
+
+        for (int mid = 0; mid < len - 1; mid++) {
             int l = mid, r = mid + 1;
+            int lSum = cookie[l], rSum = cookie[r];
             
-            while (0 <= l && r < size) {
-                if (lsum == rsum) answer = Math.max(answer, lsum);
+            while (0 <= l && r < len) {
+                if (lSum == rSum) answer = Math.max(answer, lSum);
                 
-                if (lsum <= rsum && l > 0) lsum += cookie[--l];
-                else if (lsum >= rsum && r < size - 1) rsum += cookie[++r];
+                if (0 < l && lSum <= rSum) lSum += cookie[--l];
+                else if (r < len - 1 && lSum >= rSum) rSum += cookie[++r];
                 else break;
             }
         }
