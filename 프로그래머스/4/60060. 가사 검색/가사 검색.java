dd8f7fed9e77777897e9c6aef7443b27cc047f7c
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-10-27 19:49:13 +0900

    [level 4] Title: 가사 검색, Time: 667.80 ms, Memory: 514 MB -BaekjoonHub

--- 프로그래머스/4/60060. 가사 검색/가사 검색.java
+++ 프로그래머스/4/60060. 가사 검색/가사 검색.java
@@ -2,83 +2,94 @@ import java.util.*;
 
 class Node {
     Map<Character, Node> child = new HashMap<>();
-    int pass, end;
-    
-    Node() { this.pass = 0; this.end = 0; }
+    int cnt;
 }
 
 class Trie {
-    final char WILD_CARD = '?';
     Node root;
     
     Trie() { this.root = new Node(); }
     
     void insert(String word) {
         Node node = this.root;
-        node.pass++;
+        
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
+            node.cnt++;
+            
             node.child.putIfAbsent(c, new Node());
             node = node.child.get(c);
-            node.pass++;
         }
-        
-        node.end++;
     }
     
-    int search(String query) {
+    int search(String word) {
         Node node = this.root;
         
-        for (int i = 0; i < query.length(); i++) {
-            char c = query.charAt(i);
-            if (c == WILD_CARD) return node.pass;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (c == '?') return node.cnt;
             
             node = node.child.get(c);
             if (node == null) return 0;
         }
         
-        return node.end;
+        return node.cnt;
     }
 }
 
 class Solution {
+    Map<Integer, Trie> natural = new HashMap<>();
+    Map<Integer, Trie> reverse = new HashMap<>();
+    StringBuilder sb = new StringBuilder();
+    
     public int[] solution(String[] words, String[] queries) {
-        /*
-            ? 가 포함되었다면 전부 매칭 가능
-            words 주고 찾는 키워드 담긴 배열 줄 때 키워드 매칭 단어 몇 개인지 순서대로
-        */
-        
+        insertWords(words);
+        return searchQueries(queries);
+    }
+    
+    private int[] searchQueries(String[] queries) {
         int[] answer = new int[queries.length];
-        Map<Integer, Trie> forward = new HashMap<>();
-        Map<Integer, Trie> backward = new HashMap<>();
-        
-        for (String word : words) {
-            int len = word.length();
-            forward.putIfAbsent(len, new Trie());
-            forward.get(len).insert(word);
-            
-            String reversed = new StringBuilder(word).reverse().toString();
-            backward.putIfAbsent(len, new Trie());
-            backward.get(len).insert(reversed);
-        }
         
         for (int i = 0; i < queries.length; i++) {
             String query = queries[i];
             int len = query.length();
             
-            if (!forward.containsKey(len)) {
-                answer[i] = 0;
+            Trie trie;
+            if (query.charAt(0) == '?') {
+                if (!reverse.containsKey(len)) {
+                    answer[i] = 0;
+                    continue;
+                }
+                
+                trie = reverse.get(len);
+                answer[i] = trie.search(sb.append(query).reverse().toString());
+                sb.setLength(0);
                 continue;
             }
             
-            if (query.charAt(0) == '?') {
-                String rQuery = new StringBuilder(query).reverse().toString();
-                answer[i] = backward.get(len).search(rQuery);
-            } else if (query.charAt(len - 1) == '?') {
-                answer[i] = forward.get(len).search(query);
+            if (!natural.containsKey(len)) {
+                answer[i] = 0;
+                continue;
             }
+            trie = natural.get(len);
+            answer[i] = trie.search(query);
         }
         
         return answer;
+    }
+    
+    private void insertWords(String[] words) {
+        for (String word : words) {
+            int len = word.length();
+            natural.putIfAbsent(len, new Trie());
+            reverse.putIfAbsent(len, new Trie());
+            
+            Trie nTrie = natural.get(len);
+            Trie rTrie = reverse.get(len);
+            
+            nTrie.insert(word);
+            rTrie.insert(sb.append(word).reverse().toString());
+            sb.setLength(0);
+        }
     }
 }
