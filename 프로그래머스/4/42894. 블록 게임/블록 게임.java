98201b1572ba424813b40d3c93cdb40a6e7873b6
Youngjo Jang <130957735+Jyoungjo@users.noreply.github.com>
2025-10-28 19:12:07 +0900

    [level 4] Title: 블록 게임, Time: 6.08 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/4/42894. 블록 게임/블록 게임.java
+++ 프로그래머스/4/42894. 블록 게임/블록 게임.java
@@ -1,111 +1,120 @@
 import java.util.*;
 
-class Point {
-    int y, x;
-    
-    Point(int y, int x) {
-        this.y = y;
-        this.x = x;
-    }
-}
-
 class Solution {
     final int[] dy = {-1, 0, 1, 0}, dx = {0, 1, 0, -1};
-    int[][] BOARD;
-    Map<Integer, List<int[]>> blocks = new HashMap<>();
     int N;
+    int[][] BOARD;
     
     public int solution(int[][] board) {
-        BOARD = board;
         N = board.length;
-        return search();
+        BOARD = board;
+        return play();
     }
     
-    private int search() {
-        int result = 0;
+    private int play() {
+        int cnt = 0;
+        
         while (true) {
-            boolean isRemoved = false;
-            boolean[][] used = new boolean[N][N];
+            boolean removed = false;
+            boolean[][] visited = new boolean[N][N];
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
                     int id = BOARD[i][j];
-                    if (id == 0 || used[i][j]) continue;
                     
-                    List<Point> points = bfs(i, j, id, used);
-                    if (check(points)) {
+                    if (id == 0 || visited[i][j]) continue;
+                    
+                    List<int[]> points = bfs(i, j, id, visited);
+                    if (isCanRemove(points)) {
                         remove(points);
-                        result++;
-                        isRemoved = true;
+                        removed = true;
+                        cnt++;
                     }
                 }
             }
             
-            if (!isRemoved) break;
+            if (!removed) break;
         }
         
-        return result;
+        return cnt;
     }
     
-    private List<Point> bfs(int sy, int sx, int id, boolean[][] visited) {
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(sy, sx));
-        visited[sy][sx] = true;
-        
-        List<Point> result = new ArrayList<>();
-        while (!q.isEmpty()) {
-            Point now = q.poll();
-            
-            result.add(now);
-            
-            for (int d = 0; d < 4; d++) {
-                int ny = now.y + dy[d], nx = now.x + dx[d];
-                
-                if (!isInRange(ny, nx) || visited[ny][nx] || BOARD[ny][nx] != id) continue;
-                q.add(new Point(ny, nx));
-                visited[ny][nx] = true;
-            }
-        }
-        
-        return result;
+    private void remove(List<int[]> points) {
+        for (int[] p : points) BOARD[p[0]][p[1]] = 0;
     }
     
-    private boolean isInRange(int y, int x) {
-        return 0 <= y && y < N && 0 <= x && x < N;
+    private boolean isCanRemove(List<int[]> points) {
+        int[] vals = calMaxMin(points);
+        int r1 = vals[0], r2 = vals[1], c1 = vals[2], c2 = vals[3];
+        
+        int id = BOARD[points.get(0)[0]][points.get(0)[1]];
+        List<int[]> empty = findEmpty(r1, r2, c1, c2, id);
+        if (empty.isEmpty()) return false;
+        
+        return isAllBlank(empty);
     }
     
-    private boolean check(List<Point> points) {
-        int r1 = Integer.MAX_VALUE, c1 = Integer.MAX_VALUE, r2 = Integer.MIN_VALUE, c2 = Integer.MIN_VALUE;
-        for (Point p : points) {
-            r1 = Math.min(r1, p.y);
-            c1 = Math.min(c1, p.x);
-            r2 = Math.max(r2, p.y);
-            c2 = Math.max(c2, p.x);
-        }
-        
-        int id = BOARD[points.get(0).y][points.get(0).x];
-        List<Point> empty = new ArrayList<>();
-        
-        for (int i = r1; i <= r2; i++) {
-            for (int j = c1; j <= c2; j++) {
-                int val = BOARD[i][j];
-                
-                if (val == 0) empty.add(new Point(i, j));
-                else if (val != id) return false;
-            }
-        }
-        
-        if (empty.size() != 2) return false;
-        
-        for (Point p : empty) {
-            for (int i = 0; i < p.y; i++) {
-                if (BOARD[i][p.x] != 0) return false;
+    private boolean isAllBlank(List<int[]> empty) {
+        for (int[] p : empty) {
+            for (int i = 0; i <= p[0]; i++) {
+                if (BOARD[i][p[1]] != 0) return false;
             }
         }
         
         return true;
     }
     
-    private void remove(List<Point> points) {
-        for (Point p : points) BOARD[p.y][p.x] = 0;
+    private List<int[]> findEmpty(int r1, int r2, int c1, int c2, int id) {
+        List<int[]> result = new ArrayList<>();
+        for (int r = r1; r <= r2; r++) {
+            for (int c = c1; c <= c2; c++) {
+                if (BOARD[r][c] == 0) result.add(new int[]{r, c});
+            }
+        }
+        
+        if (result.size() != 2) return Collections.emptyList();
+        
+        return result;
+    }
+    
+    private int[] calMaxMin(List<int[]> points) {
+        int r1 = Integer.MAX_VALUE, r2 = Integer.MIN_VALUE, c1 = Integer.MAX_VALUE, c2 = Integer.MIN_VALUE;
+        
+        for (int[] p : points) {
+            r1 = Math.min(r1, p[0]);
+            r2 = Math.max(r2, p[0]);
+            c1 = Math.min(c1, p[1]);
+            c2 = Math.max(c2, p[1]);
+        }
+        
+        return new int[]{r1, r2, c1, c2};
+    }
+    
+    private List<int[]> bfs(int i, int j, int id, boolean[][] visited) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{i, j});
+        visited[i][j] = true;
+        
+        List<int[]> points = new ArrayList<>();
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            int y = now[0], x = now[1];
+            
+            points.add(new int[]{y, x});
+            
+            for (int d = 0; d < 4; d++) {
+                int ny = y + dy[d], nx = x + dx[d];
+                
+                if (!isInRange(ny, nx) || visited[ny][nx] || BOARD[ny][nx] != id) continue;
+                
+                q.add(new int[]{ny, nx});
+                visited[ny][nx] = true;
+            }
+        }
+        
+        return points;
+    }
+    
+    private boolean isInRange(int y, int x) {
+        return 0 <= y && y < N && 0 <= x && x < N;
     }
 }
